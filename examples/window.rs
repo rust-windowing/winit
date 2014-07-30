66f6b003d02dc83824725d4aaec0e4a1b4f0968b
Tomaka17 <pierre.krieger1708@gmail.com>
2014-07-30 13:05:58 +0200

    The example now prints OpenGL version

--- examples/window.rs
+++ examples/window.rs
@@ -11,10 +11,19 @@ fn main() {
 
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const libc::c_void);
 
+    let version = {
+        use std::c_str::CString;
+        unsafe { CString::new(gl::GetString(gl::VERSION) as *const i8, false) }
+    };
+
+    println!("OpenGL version {}", version.as_str().unwrap());
+
     gl::ClearColor(0.0, 1.0, 0.0, 1.0);
 
     while !window.should_close() {
         println!("{}", window.wait_events());
+
+        println!("pos: {}", window.get_position());
 
         gl::Clear(gl::COLOR_BUFFER_BIT);
 
