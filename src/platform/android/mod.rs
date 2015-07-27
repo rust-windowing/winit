1f6c4d65a61b399d1a476dc19ca3d6a152856286
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-07-27 10:18:45 +0200

    Implement headless contexts on android

--- src/platform/android/mod.rs
+++ src/platform/android/mod.rs
@@ -1,36 +1,3 @@
 #![cfg(target_os = "android")]
 
 pub use api::android::*;
-
-use ContextError;
-
-pub struct HeadlessContext(i32);
-
-impl HeadlessContext {
-    /// See the docs in the crate root file.
-    pub fn new(_builder: BuilderAttribs) -> Result<HeadlessContext, CreationError> {
-        unimplemented!()
-    }
-
-    /// See the docs in the crate root file.
-    pub unsafe fn make_current(&self) -> Result<(), ContextError> {
-        unimplemented!()
-    }
-
-    /// See the docs in the crate root file.
-    pub fn is_current(&self) -> bool {
-        unimplemented!()
-    }
-
-    /// See the docs in the crate root file.
-    pub fn get_proc_address(&self, _addr: &str) -> *const () {
-        unimplemented!()
-    }
-
-    pub fn get_api(&self) -> ::Api {
-        ::Api::OpenGlEs
-    }
-}
-
-unsafe impl Send for HeadlessContext {}
-unsafe impl Sync for HeadlessContext {}
