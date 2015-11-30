rust-windowing/winit

commit 47df0e9eaa7f1231a07f86fc44426936c7a2589a

Fix misbehaving fullscreen window

src/api/x11/window.rs
@@ -436,14 +436,14 @@ impl Window {
         }
 
         // switching to fullscreen
-        if let Some(mut mode_to_switch_to) = mode_to_switch_to {
-            window_attributes |= ffi::CWOverrideRedirect;
-            unsafe {
-                (display.xf86vmode.XF86VidModeSwitchToMode)(display.display, screen_id, &mut mode_to_switch_to);
-                (display.xf86vmode.XF86VidModeSetViewPort)(display.display, screen_id, 0, 0);
-                set_win_attr.override_redirect = 1;
-            }
-        }
+        // if let Some(mut mode_to_switch_to) = mode_to_switch_to {
+        //     window_attributes |= ffi::CWOverrideRedirect;
+        //     unsafe {
+        //         (display.xf86vmode.XF86VidModeSwitchToMode)(display.display, screen_id, &mut mode_to_switch_to);
+        //         (display.xf86vmode.XF86VidModeSetViewPort)(display.display, screen_id, 0, 0);
+        //         set_win_attr.override_redirect = 1;
+        //     }
+        // }
 
         // finally creating the window
         let window = unsafe {
@@ -526,6 +526,49 @@ impl Window {
         }
 
         let is_fullscreen = window_attrs.monitor.is_some();
+
+        if is_fullscreen {
+            let state_atom = unsafe {
+                with_c_str("_NET_WM_STATE", |state|
+                    (display.xlib.XInternAtom)(display.display, state, 0)
+                )
+            };
+            let fs_atom = unsafe {
+                with_c_str("_NET_WM_STATE_FULLSCREEN", |state_fullscreen|
+                    (display.xlib.XInternAtom)(display.display, state_fullscreen, 0)
+                )
+            };
+
+            let client_message_event = ffi::XClientMessageEvent {
+                type_: ffi::ClientMessage,
+                serial: 0,
+                send_event: 1,
+                display: display.display,
+                window: window,
+                message_type: state_atom,
+                format: 32,
+                data: {
+                    let mut data = unsafe {
+                        mem::transmute::<[libc::c_long; 5], ffi::ClientMessageData>([0; 5])
+                    };
+                    data.set_long(0, 1);
+                    data.set_long(1, fs_atom as i64);
+                    data.set_long(2, 0);
+                    data
+                }
+            };
+            let mut x_event = ffi::XEvent::from(client_message_event);
+
+            unsafe {
+                (display.xlib.XSendEvent)(
+                    display.display,
+                    root,
+                    0,
+                    ffi::SubstructureRedirectMask | ffi::SubstructureNotifyMask,
+                    &mut x_event as *mut _
+                );
+            }
+        }
 
         // finish creating the OpenGL context
         let context = match context {

commit 32ed86a976c585e238e9ccfcc642e4ac70a27710

Fix compilation for 32 bit targets
Regression introduced in 47df0e9eaa7f1231a07f86fc44426936c7a2589a

Casting fullscreen_atom (which is the result from XInternAtom, i.e.
c_ulong) as i64 is obviously wrong -- the whole point of types such as
c_ulong is that long in C does *not* always have the same bit size...

Cast it as c_long instead.

While this is the most straightforward fix, I'm not sure it's the best
one: perhaps the x11 crate should offer a set_ulong() method along with
set_long(), which could be used here instead of the cast?

src/api/x11/window.rs
@@ -8,6 +8,7 @@ use std::cell::Cell;
 use std::sync::atomic::AtomicBool;
 use std::collections::VecDeque;
 use std::sync::{Arc, Mutex};
+use std::os::raw::c_long;
 
 use Api;
 use ContextError;
@@ -539,7 +540,7 @@ impl Window {
                     // This first `long` is the action; `1` means add/set following property.
                     data.set_long(0, 1);
                     // This second `long` is the property to set (fullscreen)
-                    data.set_long(1, fullscreen_atom as i64);
+                    data.set_long(1, fullscreen_atom as c_long);
                     data
                 }
             };