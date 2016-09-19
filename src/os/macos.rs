2def6d5fff55d31f7b5ca22b94303791c6b163cd
Andrey Lesnikov <ozkriff@gmail.com>
2016-09-19 21:05:48 +0300

    Fixed macos

--- src/os/macos.rs
+++ src/os/macos.rs
@@ -51,14 +51,14 @@ impl From<ActivationPolicy> for NSApplicationActivationPolicy {
 }
 
 /// Additional methods on `WindowBuilder` that are specific to MacOS.
-pub trait WindowBuilderExt<'a> {
-    fn with_activation_policy(mut self, activation_policy: ActivationPolicy) -> WindowBuilder<'a>;
+pub trait WindowBuilderExt {
+    fn with_activation_policy(mut self, activation_policy: ActivationPolicy) -> WindowBuilder;
 }
 
-impl<'a> WindowBuilderExt<'a> for WindowBuilder<'a> {
+impl WindowBuilderExt for WindowBuilder {
     /// Sets the activation policy for the window being built
     #[inline]
-    fn with_activation_policy(mut self, activation_policy: ActivationPolicy) -> WindowBuilder<'a> {
+    fn with_activation_policy(mut self, activation_policy: ActivationPolicy) -> WindowBuilder {
         self.platform_specific.activation_policy = activation_policy;
         self
     }
