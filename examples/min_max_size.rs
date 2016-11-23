b500808d1abd64f49d9efc047778e901ec241043
Corey Farwell <coreyf@rwell.org>
2016-11-23 10:51:15 -0500

    Rename example, remove debug logging.

--- examples/min_max_size.rs
+++ examples/min_max_size.rs
@@ -7,22 +7,14 @@ extern crate winit;
 #[cfg(target_os = "android")]
 android_start!(main);
 
-fn resize_callback(width: u32, height: u32) {
-    println!("Window resized to {}x{}", width, height);
-}
-
 fn main() {
     let window = winit::WindowBuilder::new()
-        .with_title("A fantastic window!")
-        .with_window_resize_callback(resize_callback)
         .with_min_dimensions(400, 200)
         .with_max_dimensions(800, 400)
         .build()
         .unwrap();
 
     for event in window.wait_events() {
-        println!("{:?}", event);
-
         match event {
             winit::Event::Closed => break,
             _ => ()
