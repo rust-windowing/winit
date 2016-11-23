f8666a05fb7ed9a68962ba6cfd0dcb561884935a
Corey Farwell <coreyf@rwell.org>
2016-11-23 09:41:53 -0500

    Add example demonstrating min/max window dimension constraints.

---
+++ examples/constraints.rs
@@ -0,0 +1,31 @@
+#[cfg(target_os = "android")]
+#[macro_use]
+extern crate android_glue;
+
+extern crate winit;
+
+#[cfg(target_os = "android")]
+android_start!(main);
+
+fn resize_callback(width: u32, height: u32) {
+    println!("Window resized to {}x{}", width, height);
+}
+
+fn main() {
+    let window = winit::WindowBuilder::new()
+        .with_title("A fantastic window!")
+        .with_window_resize_callback(resize_callback)
+        .with_min_dimensions(400, 200)
+        .with_max_dimensions(800, 400)
+        .build()
+        .unwrap();
+
+    for event in window.wait_events() {
+        println!("{:?}", event);
+
+        match event {
+            winit::Event::Closed => break,
+            _ => ()
+        }
+    }
+}
