b3820f8711ec4e8c579b67b19d264b285412409a
Kelvin Ly <kelvin.ly1618@gmail.com>
2017-02-13 11:32:13 -0500

    Rename emscripten platform to null platform

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -15,11 +15,9 @@ mod platform;
 #[cfg(target_os = "ios")]
 #[path="ios/mod.rs"]
 mod platform;
-#[cfg(target_os = "emscripten")]
-#[path="emscripten/mod.rs"]
-mod platform;
 
 #[cfg(all(not(target_os = "ios"), not(target_os = "windows"), not(target_os = "linux"),
   not(target_os = "macos"), not(target_os = "android"), not(target_os = "dragonfly"),
-  not(target_os = "freebsd"), not(target_os = "openbsd"), not(target_os = "emscripten")))]
-use this_platform_is_not_supported;
+  not(target_os = "freebsd"), not(target_os = "openbsd")))]
+#[path="null/mod.rs"]
+mod platform;
