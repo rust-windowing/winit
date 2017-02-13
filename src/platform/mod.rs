da240f15e2fa90b12f83b4b3f25976c0d0c36ee3
Kelvin Ly <kelvin.ly1618@gmail.com>
2017-02-13 10:19:46 -0500

    Add null platform for emscripten

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -15,8 +15,11 @@ mod platform;
 #[cfg(target_os = "ios")]
 #[path="ios/mod.rs"]
 mod platform;
+#[cfg(target_os = "emscripten")]
+#[path="emscripten/mod.rs"]
+mod platform;
 
 #[cfg(all(not(target_os = "ios"), not(target_os = "windows"), not(target_os = "linux"),
   not(target_os = "macos"), not(target_os = "android"), not(target_os = "dragonfly"),
-  not(target_os = "freebsd"), not(target_os = "openbsd")))]
+  not(target_os = "freebsd"), not(target_os = "openbsd"), not(target_os = "emscripten")))]
 use this_platform_is_not_supported;
