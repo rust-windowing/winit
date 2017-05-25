c8e791b402391020e21a64b4be29943850bee293
mitchmindtree <mitchell.nordine@gmail.com>
2017-05-25 23:18:56 +1000

    Add a test that checks that EventsLoopProxy impls Send

--- tests/window_proxy_send.rs
+++
@@ -1,9 +0,0 @@
-extern crate winit;
-
-#[cfg(feature = "window")]
-#[test]
-fn window_proxy_send() {
-    // ensures that `winit::WindowProxy` implements `Send`
-    fn needs_send<T:Send>() {}
-    needs_send::<winit::WindowProxy>();
-}
