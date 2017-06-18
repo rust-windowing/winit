23e9b79f14d1ac56c6d085ef6683e6f2bf7714df
mitchmindtree <mitchell.nordine@gmail.com>
2017-06-18 21:20:09 +1000

    Add monitor_id method to macos Window. Not yet tested.

--- src/platform/macos/window.rs
+++ src/platform/macos/window.rs
@@ -556,6 +556,21 @@ impl Window {
 
         Ok(())
     }
+
+    pub fn monitor_id(&self) -> super::MonitorId {
+        unsafe {
+            let screen = NSWindow::screen(*self.window);
+            let device_description = appkit::NSScreen::deviceDescription(screen);
+            let key = IdRef::new(NSString::alloc(nil).init_str("NSScreenNumber"));
+            let value: id = msg_send![device_description, objectForKey:*key];
+            if value != nil {
+                let screen_number: NSUInteger = msg_send![value, unsignedIntegerValue];
+                super::MonitorId(screen_number as u32)
+            } else {
+                super::get_primary_monitor()
+            }
+        }
+    }
 }
 
 // Convert the `cocoa::base::id` associated with a window to a usize to use as a unique identifier
