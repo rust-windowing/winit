c86cf416d5140062c09769d9596c58f000cd5e60
Victor Berger <victor.berger@m4x.org>
2017-07-19 18:53:49 +0200

    wayland: add a warning in the examples in case no window is displayed.

--- examples/proxy.rs
+++ examples/proxy.rs
@@ -10,6 +10,12 @@ fn main() {
 
     let proxy = events_loop.create_proxy();
 
+    if cfg!(target_os = "linux") {
+        println!("Running this example under wayland may not display a window at all.\n\
+                  This is normal and because this example does not actually draw anything in the window,\
+                  thus the compositor does not display it.");
+    }
+
     std::thread::spawn(move || {
         // Wake up the `events_loop` once every second.
         loop {
