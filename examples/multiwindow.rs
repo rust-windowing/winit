8f03fb7a34d6e1a720ace443d3b9ed49cf3fc011
gzp <z.p.gaal.devel@gmail.com>
2017-07-31 12:25:49 +0200

    Revert "test explicit drop"
    This reverts commit 491bc891e8319ff730f42ee2f57d01714c12cf06.

--- examples/multiwindow.rs
+++ examples/multiwindow.rs
@@ -6,14 +6,8 @@ fn main() {
     let window1 = winit::Window::new(&events_loop).unwrap();
     let window2 = winit::Window::new(&events_loop).unwrap();
     let window3 = winit::Window::new(&events_loop).unwrap();
-    let window4 = winit::Window::new(&events_loop).unwrap();
 
-    let window4_id = window4.id();
-    let mut window4_opt = Some(window4);
-
-    let mut num_windows = 4;
-
-    println!("Press any key on any window to drop the 4th window explicitly. (Testing impl Drop for Window)");
+    let mut num_windows = 3;
 
     if cfg!(target_os = "linux") {
         println!("Running this example under wayland may not display a window at all.\n\
@@ -30,8 +24,6 @@ fn main() {
                     println!("Window 2 has been closed")
                 } else if window_id == window3.id() {
                     println!("Window 3 has been closed");
-                } else if window_id == window4_id {
-                    println!("Window 4 has been closed");
                 } else {
                     unreachable!()
                 }
@@ -41,12 +33,6 @@ fn main() {
                     return winit::ControlFlow::Break;
                 }
             },
-
-            winit::Event::WindowEvent { event: winit::WindowEvent::KeyboardInput{..}, .. } => {
-                println!("Dropping window 4 explicitly");
-                window4_opt = None;
-            },
-
             _ => (),
         }
         winit::ControlFlow::Continue
