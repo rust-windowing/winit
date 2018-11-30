5708c88e4966721c72dff3b34f661284d97e95d7
acheronfail <acheronfail@gmail.com>
2018-12-01 09:24:54 +1100

    feedback: combine fullscreen examples into one example

--- examples/fullscreen.rs
+++ examples/fullscreen.rs
@@ -6,32 +6,52 @@ use winit::{ControlFlow, Event, WindowEvent};
 fn main() {
     let mut events_loop = winit::EventsLoop::new();
 
-    // enumerating monitors
+    let mut macos_use_simple_fullscreen = false;
     let monitor = {
-        for (num, monitor) in events_loop.get_available_monitors().enumerate() {
-            println!("Monitor #{}: {:?}", num, monitor.get_name());
+        #[cfg(target_os = "macos")]
+        {
+            print!("Please choose the fullscreen mode: (1) native, (2) simple");
+            io::stdout().flush().unwrap();
+
+            let mut num = String::new();
+            io::stdin().read_line(&mut num).unwrap();
+            let num = num.trim().parse().ok().expect("Please enter a number");
+            match num {
+                2 => macos_use_simple_fullscreen = true,
+                _ => {}
+            }
         }
 
-        print!("Please write the number of the monitor to use: ");
-        io::stdout().flush().unwrap();
+        // Prompt for monitor when using native fullscreen
+        if !macos_use_simple_fullscreen {
+            // enumerating monitors
+            for (num, monitor) in events_loop.get_available_monitors().enumerate() {
+                println!("Monitor #{}: {:?}", num, monitor.get_name());
+            }
 
-        let mut num = String::new();
-        io::stdin().read_line(&mut num).unwrap();
-        let num = num.trim().parse().ok().expect("Please enter a number");
-        let monitor = events_loop.get_available_monitors().nth(num).expect("Please enter a valid ID");
+            print!("Please write the number of the monitor to use: ");
+            io::stdout().flush().unwrap();
 
-        println!("Using {:?}", monitor.get_name());
+            let mut num = String::new();
+            io::stdin().read_line(&mut num).unwrap();
+            let num = num.trim().parse().ok().expect("Please enter a number");
+            let monitor = events_loop.get_available_monitors().nth(num).expect("Please enter a valid ID");
 
-        monitor
+            println!("Using {:?}", monitor.get_name());
+
+            Some(monitor)
+        } else {
+            None
+        }
     };
 
     let window = winit::WindowBuilder::new()
         .with_title("Hello world!")
-        .with_fullscreen(Some(monitor))
+        .with_fullscreen(monitor)
         .build(&events_loop)
         .unwrap();
 
-    let mut is_fullscreen = true;
+    let mut is_fullscreen = !macos_use_simple_fullscreen;
     let mut is_maximized = false;
     let mut decorations = true;
 
@@ -53,6 +73,17 @@ fn main() {
                     (winit::VirtualKeyCode::Escape, _) => return ControlFlow::Break,
                     (winit::VirtualKeyCode::F, winit::ElementState::Pressed) => {
                         is_fullscreen = !is_fullscreen;
+
+                        #[cfg(target_os = "macos")]
+                        {
+                            if macos_use_simple_fullscreen {
+                                use winit::os::macos::WindowExt;
+                                WindowExt::set_simple_fullscreen(&window, is_fullscreen);
+
+                                return ControlFlow::Continue;
+                            }
+                        }
+
                         if !is_fullscreen {
                             window.set_fullscreen(None);
                         } else {
