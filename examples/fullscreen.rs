bfbcab3a010667e74fa3cc07ac85bd4c85491e65
acheronfail <acheronfail@gmail.com>
2018-12-19 15:07:33 +1100

    feat: add macos simple fullscreen (#692)
    * feat: add macos simple fullscreen
    
    * move impl to WindowExt
    
    * feedback: remove warning, unused file and rename param
    
    * feedback: combine fullscreen examples into one example
    
    * fix: ensure decorations and maximize do not toggle while in fullscreen
    
    * fix: prevent warning on non-macos platforms
    
    * feedback: make changelog more explicit
    
    * fix: prevent unconditional construction of NSRect
    
    * fix: don't try to set_simple_fullscreen if already using native fullscreen
    
    * fix: ensure set_simple_fullscreen plays nicely with set_fullscreen
    
    * fix: do not enter native fullscreen if simple fullscreen is active

--- examples/fullscreen.rs
+++ examples/fullscreen.rs
@@ -6,34 +6,45 @@ use winit::{ControlFlow, Event, WindowEvent};
 fn main() {
     let mut events_loop = winit::EventsLoop::new();
 
-    // enumerating monitors
+    #[cfg(target_os = "macos")]
+    let mut macos_use_simple_fullscreen = false;
+
     let monitor = {
-        for (num, monitor) in events_loop.get_available_monitors().enumerate() {
-            println!("Monitor #{}: {:?}", num, monitor.get_name());
+        // On macOS there are two fullscreen modes "native" and "simple"
+        #[cfg(target_os = "macos")]
+        {
+            print!("Please choose the fullscreen mode: (1) native, (2) simple: ");
+            io::stdout().flush().unwrap();
+
+            let mut num = String::new();
+            io::stdin().read_line(&mut num).unwrap();
+            let num = num.trim().parse().ok().expect("Please enter a number");
+            match num {
+                2 => macos_use_simple_fullscreen = true,
+                _ => {}
+            }
+
+            // Prompt for monitor when using native fullscreen
+            if !macos_use_simple_fullscreen {
+                Some(prompt_for_monitor(&events_loop))
+            } else {
+                None
+            }
         }
 
-        print!("Please write the number of the monitor to use: ");
-        io::stdout().flush().unwrap();
-
-        let mut num = String::new();
-        io::stdin().read_line(&mut num).unwrap();
-        let num = num.trim().parse().ok().expect("Please enter a number");
-        let monitor = events_loop.get_available_monitors().nth(num).expect("Please enter a valid ID");
-
-        println!("Using {:?}", monitor.get_name());
-
-        monitor
+        #[cfg(not(target_os = "macos"))]
+        Some(prompt_for_monitor(&events_loop))
     };
+
+    let mut is_fullscreen = monitor.is_some();
+    let mut is_maximized = false;
+    let mut decorations = true;
 
     let window = winit::WindowBuilder::new()
         .with_title("Hello world!")
-        .with_fullscreen(Some(monitor))
+        .with_fullscreen(monitor)
         .build(&events_loop)
         .unwrap();
-
-    let mut is_fullscreen = true;
-    let mut is_maximized = false;
-    let mut decorations = true;
 
     events_loop.run_forever(|event| {
         println!("{:?}", event);
@@ -52,6 +63,18 @@ fn main() {
                 } => match (virtual_code, state) {
                     (winit::VirtualKeyCode::Escape, _) => return ControlFlow::Break,
                     (winit::VirtualKeyCode::F, winit::ElementState::Pressed) => {
+                        #[cfg(target_os = "macos")]
+                        {
+                            if macos_use_simple_fullscreen {
+                                use winit::os::macos::WindowExt;
+                                if WindowExt::set_simple_fullscreen(&window, !is_fullscreen) {
+                                    is_fullscreen = !is_fullscreen;
+                                }
+
+                                return ControlFlow::Continue;
+                            }
+                        }
+
                         is_fullscreen = !is_fullscreen;
                         if !is_fullscreen {
                             window.set_fullscreen(None);
@@ -76,4 +99,23 @@ fn main() {
 
         ControlFlow::Continue
     });
+}
+
+// Enumerate monitors and prompt user to choose one
+fn prompt_for_monitor(events_loop: &winit::EventsLoop) -> winit::MonitorId {
+    for (num, monitor) in events_loop.get_available_monitors().enumerate() {
+        println!("Monitor #{}: {:?}", num, monitor.get_name());
+    }
+
+    print!("Please write the number of the monitor to use: ");
+    io::stdout().flush().unwrap();
+
+    let mut num = String::new();
+    io::stdin().read_line(&mut num).unwrap();
+    let num = num.trim().parse().ok().expect("Please enter a number");
+    let monitor = events_loop.get_available_monitors().nth(num).expect("Please enter a valid ID");
+
+    println!("Using {:?}", monitor.get_name());
+
+    monitor
 }
