b994918177bc18f48a484e40f04ef190a99419f3
Osspial <osspial@gmail.com>
2019-02-22 09:51:08 -0500

    Remove icon loading feature

--- src/lib.rs
+++ src/lib.rs
@@ -81,8 +81,6 @@ extern crate lazy_static;
 extern crate libc;
 #[macro_use]
 extern crate log;
-#[cfg(feature = "icon_loading")]
-extern crate image;
 #[cfg(feature = "serde")]
 #[macro_use]
 extern crate serde;
