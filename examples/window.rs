72509b5b421cd72e8146e44bb38d7e227443831d
Osspial <osspial@gmail.com>
2019-06-18 11:15:55 -0400

    Update example in README.md and add move prefix to window.rs example closure (#921)
    * Update example in README.md and add move prefix to window.rs example
    
    * Make Window and README example trivially capture window
    
    * Update README.md

--- examples/window.rs
+++ examples/window.rs
@@ -6,19 +6,19 @@ use winit::event_loop::{EventLoop, ControlFlow};
 fn main() {
     let event_loop = EventLoop::new();
 
-    let _window = WindowBuilder::new()
+    let window = WindowBuilder::new()
         .with_title("A fantastic window!")
         .build(&event_loop)
         .unwrap();
 
-    event_loop.run(|event, _, control_flow| {
+    event_loop.run(move |event, _, control_flow| {
         println!("{:?}", event);
 
         match event {
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = ControlFlow::Exit,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
             _ => *control_flow = ControlFlow::Wait,
         }
     });
