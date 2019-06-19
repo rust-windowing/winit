4343454d1b5943db89cc6558b2f3eb2d5fd56888
Osspial <osspial@gmail.com>
2019-06-19 15:41:35 -0400

    Move timer_length to separate variable

--- examples/timer.rs
+++ examples/timer.rs
@@ -6,14 +6,16 @@ use winit::event_loop::{EventLoop, ControlFlow};
 fn main() {
     let event_loop = EventLoop::new();
 
+    let timer_length = Duration::new(1, 0);
+
     event_loop.run(move |event, _, control_flow| {
         println!("{:?}", event);
 
         match event {
             Event::NewEvents(StartCause::Init) =>
-                *control_flow = ControlFlow::WaitUntil(Instant::now() + Duration::new(1, 0)),
+                *control_flow = ControlFlow::WaitUntil(Instant::now() + timer_length),
             Event::NewEvents(StartCause::ResumeTimeReached{..}) => {
-                *control_flow = ControlFlow::WaitUntil(Instant::now() + Duration::new(1, 0));
+                *control_flow = ControlFlow::WaitUntil(Instant::now() + timer_length);
                 println!("\nTimer\n");
             },
             Event::WindowEvent {
