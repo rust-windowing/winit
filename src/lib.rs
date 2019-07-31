03a8d118aa6173837da5b2311afbb8484f7a6a80
mtak- <mtak-@users.noreply.github.com>
2019-07-30 23:57:31 -0700

    iOS: add support for controlling the home indicator, and Exclusive video mode (#1078)
    * iOS: platform specific edge home indicator control
    
    * iOS: exclusive video mode support
    
    * address nits, and linkify all the ios documentation

--- src/lib.rs
+++ src/lib.rs
@@ -120,7 +120,7 @@ extern crate serde;
 #[macro_use]
 extern crate derivative;
 #[macro_use]
-#[cfg(target_os = "windows")]
+#[cfg(any(target_os = "ios", target_os = "windows"))]
 extern crate bitflags;
 #[cfg(any(target_os = "macos", target_os = "ios"))]
 #[macro_use]
