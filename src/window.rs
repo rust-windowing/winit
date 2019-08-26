a527e72a096c613b66453ecbdcd8ae47e18dfecb
Osspial <osspial@gmail.com>
2019-08-26 19:49:01 -0400

    Add docs

--- src/window.rs
+++ src/window.rs
@@ -413,7 +413,8 @@ impl Window {
 
     /// Modifies the position of the window.
     ///
-    /// See `outer_position` for more information about the coordinates.
+    /// See `outer_position` for more information about the coordinates. This automatically un-maximizes the
+    /// window if it's maximized.
     ///
     /// This is a no-op if the window has already been closed.
     ///
@@ -445,7 +446,8 @@ impl Window {
 
     /// Modifies the inner size of the window.
     ///
-    /// See `inner_size` for more information about the values.
+    /// See `inner_size` for more information about the values. This automatically un-maximizes the
+    /// window if it's maximized.
     ///
     /// ## Platform-specific
     ///
