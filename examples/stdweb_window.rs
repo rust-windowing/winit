164d3fed3e9925fc7fe442176e89eab622b6735c
Ryan Goldstein <ryan@ryanisaacg.com>
2019-10-25 09:38:19 -0400

    Add an example showing how to use stdweb
    The existing `window` example requires a little bit more plumbing to use
    on the web

---
+++ examples/stdweb_window.rs
@@ -0,0 +1,36 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    platform::web::WindowExtStdweb,
+    window::WindowBuilder,
+};
+// Note: stdweb is aliased to std_web in winit's Cargo.toml
+// In most cases, it should not be necessary to include this line
+use std_web as stdweb;
+use stdweb::{
+    traits::*,
+    web::document
+};
+
+fn main() {
+    let event_loop = EventLoop::new();
+
+    let window = WindowBuilder::new()
+        .with_title("A fantastic window!")
+        .build(&event_loop)
+        .unwrap();
+
+    document().body().unwrap().append_child(&window.canvas());
+
+    event_loop.run(move |event, _, control_flow| {
+        stdweb::console!(log, format!("{:?}", event));
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            _ => *control_flow = ControlFlow::Wait,
+        }
+    });
+}
