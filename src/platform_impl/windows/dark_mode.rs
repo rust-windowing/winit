62932be76d3776ebea8910406d9d1c53a9f63424
David Hewitt <1939362+davidhewitt@users.noreply.github.com>
2019-11-27 21:34:47 +0000

    Change implementation of windows dark mode support

--- src/platform_impl/windows/dark_mode.rs
+++ src/platform_impl/windows/dark_mode.rs
@@ -1,48 +1,19 @@
 /// This is a simple implementation of support for Windows Dark Mode,
-/// which is a more or less straight translation of the implemenation
-/// in Windows Terminal (which was originally in C++).
+/// which is inspired by the solution in https://github.com/ysc3839/win32-darkmode
 use std::ffi::OsStr;
 use std::os::windows::ffi::OsStrExt;
 
 use winapi::{
     shared::{
+        basetsd::SIZE_T,
         minwindef::{BOOL, DWORD, UINT, ULONG, WORD},
-        ntdef::{LPSTR, WCHAR},
+        ntdef::{LPSTR, PVOID, WCHAR},
         windef::HWND,
     },
-    um::{dwmapi, libloaderapi, uxtheme, winuser},
+    um::{libloaderapi, uxtheme, winuser},
 };
 
-const UXTHEME_DLL_NAME: &'static str = "uxtheme.dll";
-const UXTHEME_SHOULDAPPSUSEDARKMODE_ORDINAL: WORD = 132;
-
-type ShouldAppsUseDarkMode = unsafe extern "system" fn() -> bool;
-
 lazy_static! {
-    static ref SHOULD_APPS_USE_DARK_MODE: ShouldAppsUseDarkMode = {
-        unsafe {
-            let module = libloaderapi::LoadLibraryExW(
-                widestring(UXTHEME_DLL_NAME).as_ptr(),
-                std::ptr::null_mut(),
-                libloaderapi::LOAD_LIBRARY_SEARCH_SYSTEM32,
-            );
-
-            let handle = libloaderapi::GetProcAddress(
-                module,
-                winuser::MAKEINTRESOURCEA(UXTHEME_SHOULDAPPSUSEDARKMODE_ORDINAL),
-            );
-
-            if handle.is_null() {
-                unsafe extern "system" fn always_false() -> bool {
-                    false
-                }
-                always_false
-            } else {
-                std::mem::transmute(handle)
-            }
-        }
-    };
-
     static ref WIN10_BUILD_VERSION: Option<DWORD> = {
         unsafe {
             let module = libloaderapi::LoadLibraryExW(
@@ -98,24 +69,12 @@ lazy_static! {
         }
     };
 
-    // The DWMA_USE_IMMERSIVE_DARK_MODE attribute is undocumented, and
-    // unfortunately hasn't remained stable despite usage inside the
-    // public source of the Windows Terminal project.
-    //
-    // This mapping may need to be updated in the future if the
-    // attribute changes again.
-    static ref DWMWA_USE_IMMERSIVE_DARK_MODE: Option<DWORD> = {
-        match &*WIN10_BUILD_VERSION {
-            &Some(build_version) => {
-                if build_version >= 17763 && build_version <= 18362  {
-                    Some(19)
-                } else if build_version > 18362 {
-                    Some(20)
-                } else {
-                    None
-                }
-            },
-            None => None
+    static ref DARK_MODE_SUPPORTED: bool = {
+        // We won't try to do anything for windows versions < 17763
+        // (Windows 10 October 2018 update)
+        match *WIN10_BUILD_VERSION {
+            Some(v) => v > 18362,
+            None => false
         }
     };
 
@@ -126,11 +85,8 @@ lazy_static! {
 /// Attempt to set dark mode on a window, if necessary.
 /// Returns true if dark mode was set, false if not.
 pub fn try_dark_mode(hwnd: HWND) -> bool {
-    if let &Some(attribute) = &*DWMWA_USE_IMMERSIVE_DARK_MODE {
-        // According to Windows Terminal source, should be BOOL (32-bit int)
-        // to be appropriately sized as a parameter for DwmSetWindowAttribute
+    if *DARK_MODE_SUPPORTED {
         let is_dark_mode = should_use_dark_mode();
-        let is_dark_mode_bigbool = is_dark_mode as BOOL;
 
         let theme_name = if is_dark_mode {
             DARK_THEME_NAME.as_ptr()
@@ -144,15 +100,7 @@ pub fn try_dark_mode(hwnd: HWND) -> bool {
                 uxtheme::SetWindowTheme(hwnd, theme_name as _, std::ptr::null())
             );
 
-            assert_eq!(
-                0,
-                dwmapi::DwmSetWindowAttribute(
-                    hwnd,
-                    attribute,
-                    &is_dark_mode_bigbool as *const _ as _,
-                    std::mem::size_of_val(&is_dark_mode_bigbool) as _
-                )
-            );
+            set_dark_mode_for_window(hwnd, is_dark_mode)
         }
 
         is_dark_mode
@@ -161,11 +109,111 @@ pub fn try_dark_mode(hwnd: HWND) -> bool {
     }
 }
 
+fn set_dark_mode_for_window(hwnd: HWND, is_dark_mode: bool) {
+    // Uses Windows undocumented API SetWindowCompositionAttribute,
+    // as seen in win32-darkmode example linked at top of file.
+
+    type SetWindowCompositionAttribute =
+        unsafe extern "system" fn(HWND, *mut WINDOWCOMPOSITIONATTRIBDATA) -> BOOL;
+
+    #[allow(non_snake_case)]
+    type WINDOWCOMPOSITIONATTRIB = u32;
+    const WCA_USEDARKMODECOLORS: WINDOWCOMPOSITIONATTRIB = 26;
+
+    #[allow(non_snake_case)]
+    #[repr(C)]
+    struct WINDOWCOMPOSITIONATTRIBDATA
+    {
+        Attrib: WINDOWCOMPOSITIONATTRIB,
+        pvData: PVOID,
+        cbData: SIZE_T
+    }
+
+    lazy_static! {
+        static ref SET_WINDOW_COMPOSITION_ATTRIBUTE:
+        SetWindowCompositionAttribute = {
+            unsafe {
+                let module = libloaderapi::LoadLibraryExW(
+                    widestring("user32.dll").as_ptr(),
+                    std::ptr::null_mut(),
+                    libloaderapi::LOAD_LIBRARY_SEARCH_SYSTEM32,
+                );
+
+                let handle = libloaderapi::GetProcAddress(
+                    module,
+                    "SetWindowCompositionAttribute\0".as_ptr() as _,
+                );
+
+                if handle.is_null() {
+                    unsafe extern "system" fn always_false(
+                        _: HWND,
+                        _: *mut WINDOWCOMPOSITIONATTRIBDATA
+                    ) -> BOOL {
+                        false.into()
+                    }
+                    always_false
+                } else {
+                    std::mem::transmute(handle)
+                }
+            }
+        };
+    }
+
+    unsafe {
+        // SetWindowCompositionAttribute needs a bigbool (i32), not bool.
+        let mut is_dark_mode_bigbool = is_dark_mode as BOOL;
+
+        let mut data = WINDOWCOMPOSITIONATTRIBDATA {
+            Attrib: WCA_USEDARKMODECOLORS,
+            pvData: &mut is_dark_mode_bigbool as *mut _ as _,
+            cbData: std::mem::size_of_val(&is_dark_mode_bigbool) as _
+        };
+
+        assert_eq!(
+            1,
+            SET_WINDOW_COMPOSITION_ATTRIBUTE(
+                hwnd,
+                &mut data as *mut _
+            )
+        );
+    }
+}
+
 fn should_use_dark_mode() -> bool {
     should_apps_use_dark_mode() && !is_high_contrast()
 }
 
 fn should_apps_use_dark_mode() -> bool {
+    type ShouldAppsUseDarkMode = unsafe extern "system" fn() -> bool;
+    lazy_static! {
+        static ref SHOULD_APPS_USE_DARK_MODE: ShouldAppsUseDarkMode = {
+            unsafe {
+
+                const UXTHEME_SHOULDAPPSUSEDARKMODE_ORDINAL: WORD = 132;
+
+                let module = libloaderapi::LoadLibraryExW(
+                    widestring("uxtheme.dll").as_ptr(),
+                    std::ptr::null_mut(),
+                    libloaderapi::LOAD_LIBRARY_SEARCH_SYSTEM32,
+                );
+
+                let handle = libloaderapi::GetProcAddress(
+                    module,
+                    winuser::MAKEINTRESOURCEA(UXTHEME_SHOULDAPPSUSEDARKMODE_ORDINAL),
+                );
+
+                if handle.is_null() {
+                    unsafe extern "system" fn always_false() -> bool {
+                        false
+                    }
+                    always_false
+                } else {
+                    std::mem::transmute(handle)
+                }
+            }
+        };
+    }
+
     unsafe { SHOULD_APPS_USE_DARK_MODE() }
 }
 
