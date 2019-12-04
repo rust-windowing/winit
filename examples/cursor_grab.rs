362e8017ad6387cde084b0ccf46d5194913e696e
Osspial <osspial@gmail.com>
2019-12-03 19:01:48 -0500

    Make examples work

--- examples/cursor_grab.rs
+++ examples/cursor_grab.rs
@@ -1,5 +1,5 @@
 use winit::{
-    event::{DeviceEvent, ElementState, Event, KeyboardInput, WindowEvent},
+    event::{DeviceEvent, ElementState, Event, ModifiersState, KeyboardInput, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
@@ -28,10 +28,11 @@ fn main() {
                     ..
                 } => {
                     use winit::event::VirtualKeyCode::*;
+                    println!("{:?}", modifiers);
                     match key {
                         Escape => *control_flow = ControlFlow::Exit,
-                        G => window.set_cursor_grab(!modifiers.shift).unwrap(),
-                        H => window.set_cursor_visible(modifiers.shift),
+                        G => window.set_cursor_grab(!modifiers.contains(ModifiersState::SHIFT)).unwrap(),
+                        H => window.set_cursor_visible(modifiers.contains(ModifiersState::SHIFT)),
                         _ => (),
                     }
                 }
--- examples/multithreaded.rs
+++ examples/multithreaded.rs
@@ -5,7 +5,7 @@ fn main() {
     use std::{collections::HashMap, sync::mpsc, thread, time::Duration};
 
     use winit::{
-        event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+        event::{ElementState, Event, ModifiersState, KeyboardInput, VirtualKeyCode, WindowEvent},
         event_loop::{ControlFlow, EventLoop},
         window::{CursorIcon, Fullscreen, WindowBuilder},
     };
@@ -60,7 +60,7 @@ fn main() {
                         ..
                     } => {
                         window.set_title(&format!("{:?}", key));
-                        let state = !modifiers.shift;
+                        let state = !modifiers.contains(ModifiersState::SHIFT);
                         use VirtualKeyCode::*;
                         match key {
                             A => window.set_always_on_top(state),
@@ -81,7 +81,7 @@ fn main() {
                                     video_modes.iter().nth(video_mode_id).unwrap()
                                 );
                             }
-                            F => window.set_fullscreen(match (state, modifiers.alt) {
+                            F => window.set_fullscreen(match (state, modifiers.contains(ModifiersState::ALT)) {
                                 (true, false) => {
                                     Some(Fullscreen::Borderless(window.current_monitor()))
                                 }
