01449f5b7ded6b49f6e713e01d7c24a5bf223367
Francesca Plebani <francesca@brainiumstudios.com>
2019-12-12 12:11:00 -0800

    Remove cruft I left here a year ago

--- src/platform_impl/macos/app_delegate.rs
+++ src/platform_impl/macos/app_delegate.rs
@@ -28,28 +28,8 @@ lazy_static! {
             did_finish_launching as extern "C" fn(&Object, Sel, id) -> BOOL,
         );
         decl.add_method(
-            sel!(applicationDidBecomeActive:),
-            did_become_active as extern "C" fn(&Object, Sel, id),
-        );
-        decl.add_method(
-            sel!(applicationWillResignActive:),
-            will_resign_active as extern "C" fn(&Object, Sel, id),
-        );
-        decl.add_method(
             sel!(applicationDidResignActive:),
             did_resign_active as extern "C" fn(&mut Object, Sel, id),
-        );
-        decl.add_method(
-            sel!(applicationWillEnterForeground:),
-            will_enter_foreground as extern "C" fn(&Object, Sel, id),
-        );
-        decl.add_method(
-            sel!(applicationDidEnterBackground:),
-            did_enter_background as extern "C" fn(&Object, Sel, id),
-        );
-        decl.add_method(
-            sel!(applicationWillTerminate:),
-            will_terminate as extern "C" fn(&Object, Sel, id),
         );
 
         // Normally when you run or distribute a macOS app, it's bundled:
@@ -115,7 +95,7 @@ extern "C" fn new(this: &Object, _: Sel, _: id) -> id {
 }
 
 extern "C" fn did_finish_launching(this: &Object, _: Sel, _: id) -> BOOL {
-    trace!("Triggered `didFinishLaunching`");
+    trace!("Triggered `did_finish_launching`");
     unsafe {
         if let None = util::app_name() {
             // This app is unbundled, so we need to do some shenanigans for the
@@ -135,66 +115,14 @@ extern "C" fn did_finish_launching(this: &Object, _: Sel, _: id) -> BOOL {
         }
     }
     AppState::launched();
-    trace!("Completed `didFinishLaunching`");
+    trace!("Completed `did_finish_launching`");
     YES
 }
 
-extern "C" fn did_become_active(_: &Object, _: Sel, _: id) {
-    trace!("Triggered `didBecomeActive`");
-    /*unsafe {
-        HANDLER.lock().unwrap().handle_nonuser_event(Event::Resumed)
-    }*/
-    trace!("Completed `didBecomeActive`");
-}
-
-extern "C" fn will_resign_active(_: &Object, _: Sel, _: id) {
-    trace!("Triggered `willResignActive`");
-    /*unsafe {
-        HANDLER.lock().unwrap().handle_nonuser_event(Event::Suspended)
-    }*/
-    trace!("Completed `willResignActive`");
-}
-
 extern "C" fn did_resign_active(this: &mut Object, _: Sel, _: id) {
-    trace!("Triggered `didResignActive`");
+    trace!("Triggered `did_resign_active`");
     activation_hack_refocus(this);
-    trace!("Completed `didResignActive`");
-}
-
-extern "C" fn will_enter_foreground(_: &Object, _: Sel, _: id) {
-    trace!("Triggered `willEnterForeground`");
-    trace!("Completed `willEnterForeground`");
-}
-
-extern "C" fn did_enter_background(_: &Object, _: Sel, _: id) {
-    trace!("Triggered `didEnterBackground`");
-    trace!("Completed `didEnterBackground`");
-}
-
-extern "C" fn will_terminate(_: &Object, _: Sel, _: id) {
-    trace!("Triggered `willTerminate`");
-    /*unsafe {
-        let app: id = msg_send![class!(UIApplication), sharedApplication];
-        let windows: id = msg_send![app, windows];
-        let windows_enum: id = msg_send![windows, objectEnumerator];
-        let mut events = Vec::new();
-        loop {
-            let window: id = msg_send![windows_enum, nextObject];
-            if window == nil {
-                break
-            }
-            let is_winit_window: BOOL = msg_send![window, isKindOfClass:class!(WinitUIWindow)];
-            if is_winit_window == YES {
-                events.push(Event::WindowEvent {
-                    window_id: RootWindowId(window.into()),
-                    event: WindowEvent::Destroyed,
-                });
-            }
-        }
-        HANDLER.lock().unwrap().handle_nonuser_events(events);
-        HANDLER.lock().unwrap().terminated();
-    }*/
-    trace!("Completed `willTerminate`");
+    trace!("Completed `did_resign_active`");
 }
 
 static ACTIVATION_HACK_FLAG: &'static str = "activationHackFlag";
@@ -209,7 +137,7 @@ unsafe fn get_activation_hack_flag(this: &Object) -> bool {
 
 // First, we switch focus to the dock.
 extern "C" fn activation_hack_unfocus(this: &mut Object, _: Sel, _: id) {
-    trace!("Triggered `activationHackUnfocus`");
+    trace!("Triggered `activation_hack_unfocus`");
     unsafe {
         // We only perform the hack if the app failed to activate, since
         // otherwise, there'd be a gross (but fast) flicker as it unfocused and
@@ -249,12 +177,12 @@ extern "C" fn activation_hack_unfocus(this: &mut Object, _: Sel, _: id) {
             }
         }
     }
-    trace!("Completed `activationHackUnfocus`");
+    trace!("Completed `activation_hack_unfocus`");
 }
 
 // Then, we switch focus back to our window, and the user rejoices!
 extern "C" fn activation_hack_refocus(this: &mut Object) {
-    trace!("Triggered `activationHackRefocus`");
+    trace!("Triggered `activation_hack_refocus`");
     unsafe {
         if get_activation_hack_flag(this) {
             set_activation_hack_flag(this, false);
@@ -270,5 +198,5 @@ extern "C" fn activation_hack_refocus(this: &mut Object) {
             }
         }
     }
-    trace!("Completed `activationHackRefocus`");
+    trace!("Completed `activation_hack_refocus`");
 }
