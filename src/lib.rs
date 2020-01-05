7bc2f8039d697efecf357f6e51ab70055a9e70d5
Osspial <osspial@gmail.com>
2020-01-05 02:03:36 -0500

    Update root docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-//! Winit allows you to build a window on as many platforms as possible.
+//! Winit is a cross-platform window creation and event loop management library.
 //!
-//! # Building a window
+//! # Building windows
 //!
 //! Before you can build a [`Window`], you first need to build an [`EventLoop`]. This is done with the
 //! [`EventLoop::new()`] function.
@@ -15,26 +15,24 @@
 //!  - Calling [`Window::new(&event_loop)`][window_new].
 //!  - Calling [`let builder = WindowBuilder::new()`][window_builder_new] then [`builder.build(&event_loop)`][window_builder_build].
 //!
-//! The first way is the simplest way and will give you default values for everything.
-//!
-//! The second way allows you to customize the way your [`Window`] will look and behave by modifying
-//! the fields of the [`WindowBuilder`] object before you create the [`Window`].
+//! The first method is the simplest, and will give you default values for everything. The second
+//! method allows you to customize the way your [`Window`] will look and behave by modifying the
+//! fields of the [`WindowBuilder`] object before you create the [`Window`].
 //!
 //! # Event handling
 //!
 //! Once a [`Window`] has been created, it will generate different *events*. A [`Window`] object can
-//! generate a [`WindowEvent`] when certain things happen, like whenever the user moves their mouse
-//! or presses a key inside the [`Window`]. Devices can generate a [`DeviceEvent`] directly as well,
-//! which contains unfiltered event data that isn't specific to a certain window. Some user
-//! activity, like mouse movement, can generate both a [`WindowEvent`] *and* a [`DeviceEvent`]. You
-//! can also create and handle your own custom [`UserEvent`]s, if desired.
+//! generate [`WindowEvent`]s when certain input events occur, such as a cursor moving over the
+//! window or a key getting pressed while the window is focused. Devices can generate
+//! [`DeviceEvent`]s, which contain unfiltered event data that isn't specific to a certain window.
+//! Some user activity, like mouse movement, can generate both a [`WindowEvent`] *and* a
+//! [`DeviceEvent`]. You can also create and handle your own custom [`UserEvent`]s, if desired.
 //!
-//! Events can be retreived by using an [`EventLoop`]. A [`Window`] will send its events to the
-//! [`EventLoop`] object it was created with.
-//!
-//! You do this by calling [`event_loop.run(...)`][event_loop_run]. This function will run forever
-//! unless the `control_flow` argument given to the closure is set to [`ControlFlow`]`::`[`Exit`],
-//! at which point [`Event`]`::`[`LoopDestroyed`] is emitted and the entire program terminates.
+//! You can retreive events by calling [`EventLoop::run`][event_loop_run]. This function will
+//! dispatch events for every [`Window`] that was created with that particular [`EventLoop`], and
+//! will run until the `control_flow` argument given to the closure is set to
+//! [`ControlFlow`]`::`[`Exit`], at which point [`Event`]`::`[`LoopDestroyed`] is emitted and the
+//! entire program terminates.
 //!
 //! Winit no longer uses a `EventLoop::poll_events() -> impl Iterator<Event>`-based event loop
 //! model, since that can't be implemented properly on web and mobile platforms and works poorly on
@@ -54,7 +52,23 @@
 //! let window = WindowBuilder::new().build(&event_loop).unwrap();
 //!
 //! event_loop.run(move |event, _, control_flow| {
+//!     // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
+//!     // dispatched any events. This is ideal for games and similar applications.
+//!     *control_flow = ControlFlow::Poll;
+//!
+//!     // ControlFlow::Wait pauses the event loop if no events are available to process.
+//!     // This is ideal for non-game applications that only update in response to user
+//!     // input, and uses significantly less power/CPU time than ControlFlow::Poll.
+//!     *control_flow = ControlFlow::Wait;
+//!
 //!     match event {
+//!         Event::WindowEvent {
+//!             event: WindowEvent::CloseRequested,
+//!             ..
+//!         } => {
+//!             println!("The close button was pressed; stopping");
+//!             *control_flow = ControlFlow::Exit
+//!         },
 //!         Event::MainEventsCleared => {
 //!             // Application update code.
 //!
@@ -68,33 +82,20 @@
 //!             // rendering in here allows the program to gracefully handle redraws requested
 //!             // by the OS.
 //!         },
-//!         Event::WindowEvent {
-//!             event: WindowEvent::CloseRequested,
-//!             ..
-//!         } => {
-//!             println!("The close button was pressed; stopping");
-//!             *control_flow = ControlFlow::Exit
-//!         },
-//!         // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
-//!         // dispatched any events. This is ideal for games and similar applications.
-//!         _ => *control_flow = ControlFlow::Poll,
-//!         // ControlFlow::Wait pauses the event loop if no events are available to process.
-//!         // This is ideal for non-game applications that only update in response to user
-//!         // input, and uses significantly less power/CPU time than ControlFlow::Poll.
-//!         // _ => *control_flow = ControlFlow::Wait,
+//!         _ => ()
 //!     }
 //! });
 //! ```
 //!
-//! If you use multiple [`Window`]s, [`Event`]`::`[`WindowEvent`] has a member named `window_id`. You can
-//! compare it with the value returned by the [`id()`][window_id_fn] method of [`Window`] in order to know which
-//! [`Window`] has received the event.
+//! [`Event`]`::`[`WindowEvent`] has a [`WindowId`] member. In multi-window environments, it should be
+//! compared to the value returned by [`Window::id()`][window_id_fn] to determine which [`Window`]
+//! dispatched the event.
 //!
 //! # Drawing on the window
 //!
-//! Winit doesn't provide any function that allows drawing on a [`Window`]. However it allows you to
+//! Winit doesn't directly provide any methods for drawing on a [`Window`]. However it allows you to
 //! retrieve the raw handle of the window (see the [`platform`] module), which in turn allows you
-//! to create an OpenGL/Vulkan/DirectX/Metal/etc. context that will draw on the [`Window`].
+//! to create an OpenGL/Vulkan/DirectX/Metal/etc. context that can be used to render graphics.
 //!
 //! [`EventLoop`]: event_loop::EventLoop
 //! [`EventLoopExtDesktop::run_return`]: ./platform/desktop/trait.EventLoopExtDesktop.html#tymethod.run_return
@@ -103,6 +104,7 @@
 //! [`ControlFlow`]: event_loop::ControlFlow
 //! [`Exit`]: event_loop::ControlFlow::Exit
 //! [`Window`]: window::Window
+//! [`WindowId`]: window::WindowId
 //! [`WindowBuilder`]: window::WindowBuilder
 //! [window_new]: window::Window::new
 //! [window_builder_new]: window::WindowBuilder::new
