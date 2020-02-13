84b9debdf497e35415f23b995971b8e7fc6b67b4
Christian Duerr <contact@christianduerr.com>
2020-02-13 17:22:58 +0000

    Fix dark mode success reply

--- src/platform_impl/windows/dark_mode.rs
+++ src/platform_impl/windows/dark_mode.rs
@@ -126,7 +126,7 @@ fn set_dark_mode_for_window(hwnd: HWND, is_dark_mode: bool) -> bool {
 
             let status = set_window_composition_attribute(hwnd, &mut data as *mut _);
 
-            status == 1
+            status != 0
         }
     } else {
         false
