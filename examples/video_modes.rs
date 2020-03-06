47284ea45d3fbac07a9af048b97af18a16d4c817
Osspial <osspial@gmail.com>
2020-03-06 16:26:41 -0500

    List video modes in CI

--- examples/video_modes.rs
+++ examples/video_modes.rs
@@ -3,11 +3,13 @@ use winit::event_loop::EventLoop;
 fn main() {
     simple_logger::init().unwrap();
     let event_loop = EventLoop::new();
-    let monitor = event_loop.primary_monitor();
 
     println!("Listing available video modes:");
 
-    for mode in monitor.video_modes() {
-        println!("{}", mode);
+    for monitor in event_loop.available_monitors() {
+        println!("{:?}", monitor);
+        for mode in monitor.video_modes() {
+            println!("{}", mode);
+        }
     }
 }
