d1310814c1190cd85523f573243275f57c1352a2
Osspial <osspial@gmail.com>
2020-03-07 18:16:33 -0500

    Use i32 instead of u32 for position type in WindowEvent::Moved

--- src/platform_impl/windows/event_loop.rs
+++ src/platform_impl/windows/event_loop.rs
@@ -736,7 +736,7 @@ unsafe extern "system" fn public_window_callback<T: 'static>(
             let windowpos = lparam as *const winuser::WINDOWPOS;
             if (*windowpos).flags & winuser::SWP_NOMOVE != winuser::SWP_NOMOVE {
                 let physical_position =
-                    PhysicalPosition::new((*windowpos).x as u32, (*windowpos).y as u32);
+                    PhysicalPosition::new((*windowpos).x as i32, (*windowpos).y as i32);
                 subclass_input.send_event(Event::WindowEvent {
                     window_id: RootWindowId(WindowId(window)),
                     event: Moved(physical_position),
