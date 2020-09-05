ac831186b6bbef0b71f560d38a13462179de4e73
Michael Kirk <michael.code@endoftheworl.de>
2020-09-04 19:19:02 -0700

    Update src/platform_impl/macos/view.rs
    Co-authored-by: Kirill Chibisov <contact@kchibisov.com>

--- src/platform_impl/macos/view.rs
+++ src/platform_impl/macos/view.rs
@@ -1003,7 +1003,7 @@ extern "C" fn scroll_wheel(this: &Object, _sel: Sel, event: id) {
         let state = &mut *(state_ptr as *mut ViewState);
 
         let delta = {
-            // macos horizontal sign convention is the inverse of winit
+            // macOS horizontal sign convention is the inverse of winit.
             let (x, y) = (event.scrollingDeltaX() * -1.0, event.scrollingDeltaY());
             if event.hasPreciseScrollingDeltas() == YES {
                 let delta = LogicalPosition::new(x, y).to_physical(state.get_scale_factor());
