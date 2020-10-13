34ee46e205fc73f4c2d5206600b24bfc5fc4b49f
VZout <viktor.zoutman@embark-studios.com>
2020-10-13 16:03:53 +0200

    fixed a comment

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -126,7 +126,7 @@ pub trait WindowBuilderExtWindows {
     /// See https://docs.microsoft.com/en-us/windows/win32/api/objbase/nf-objbase-coinitialize#remarks for more information.
     fn with_drag_and_drop(self, flag: bool) -> WindowBuilder;
 
-    /// Forces a theme or use the system settings if None is provided.
+    /// Request a preferred theme.
     fn with_theme(self, theme: Theme) -> WindowBuilder;
 }
 
