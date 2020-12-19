884e53f6a1017387d3598fe408b96381ece92366
Simas Toleikis <simast@wix.com>
2020-12-19 14:19:08 +0200

    Ignore more platforms.

--- src/platform_impl/ios/window.rs
+++ src/platform_impl/ios/window.rs
@@ -190,6 +190,11 @@ impl Inner {
         warn!("`Window::set_maximized` is ignored on iOS")
     }
 
+    pub fn maximized(&self) -> bool {
+        warn!("`Window::maximized` is ignored on iOS");
+        false
+    }
+
     pub fn set_fullscreen(&self, monitor: Option<Fullscreen>) {
         unsafe {
             let uiscreen = match monitor {
