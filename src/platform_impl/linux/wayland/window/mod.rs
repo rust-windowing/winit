1002f30b067c6931d83fc91dd855512eb1af2e2c
daxpedda <daxpedda@gmail.com>
2021-01-26 20:20:27 +0100

    X11 implementation.

--- src/platform_impl/linux/wayland/window/mod.rs
+++ src/platform_impl/linux/wayland/window/mod.rs
@@ -587,6 +587,11 @@ impl Window {
     }
 
     #[inline]
+    pub fn set_drag_window(&self) -> Result<(), ExternalError> {
+        Err(ExternalError::NotSupported(NotSupportedError::new()))
+    }
+
+    #[inline]
     pub fn set_ime_position(&self, position: Position) {
         let scale_factor = self.scale_factor() as f64;
         let position = position.to_logical(scale_factor);
