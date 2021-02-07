8dd20ea0b95e9ae89b7d24301affbdb2fb9d33da
daxpedda <daxpedda@gmail.com>
2021-02-07 17:53:42 +0100

    Add multi-window example.

--- examples/drag_window.rs
+++ examples/drag_window.rs
@@ -1,6 +1,8 @@
 use simple_logger::SimpleLogger;
 use winit::{
-    event::{ElementState, Event, MouseButton, WindowEvent},
+    event::{
+        ElementState, Event, KeyboardInput, MouseButton, StartCause, VirtualKeyCode, WindowEvent,
+    },
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
@@ -9,26 +11,46 @@ fn main() {
     SimpleLogger::new().init().unwrap();
     let event_loop = EventLoop::new();
 
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let window_1 = WindowBuilder::new().build(&event_loop).unwrap();
+    let window_2 = WindowBuilder::new().build(&event_loop).unwrap();
 
-    event_loop.run(move |event, _, control_flow| {
-        println!("{:?}", event);
+    let mut switched = false;
 
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::NewEvents(StartCause::Init) => {
+            eprintln!("Switch which window is to be dragged by pressing \"x\".")
+        }
+        Event::WindowEvent { event, window_id } => match event {
+            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+            WindowEvent::MouseInput {
+                state: ElementState::Pressed,
+                button: MouseButton::Left,
                 ..
-            } => *control_flow = ControlFlow::Exit,
-            Event::WindowEvent {
-                event:
-                    WindowEvent::MouseInput {
-                        state: ElementState::Pressed,
-                        button: MouseButton::Left,
+            } => {
+                let window = if (window_id == window_1.id() && switched)
+                    || (window_id == window_2.id() && !switched)
+                {
+                    &window_2
+                } else {
+                    &window_1
+                };
+
+                window.set_drag_window().unwrap()
+            }
+            WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Released,
+                        virtual_keycode: Some(VirtualKeyCode::X),
                         ..
                     },
                 ..
-            } => window.set_drag_window().unwrap(),
+            } => {
+                switched = !switched;
+                println!("Switched!")
+            }
             _ => (),
-        }
+        },
+        _ => (),
     });
 }
