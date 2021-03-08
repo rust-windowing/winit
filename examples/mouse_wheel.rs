5095add3cbb87c6d12114567069b0f5129a48364
Norbert Nemec <Norbert@Nemec-online.de>
2021-03-08 19:20:10 +0100

    move window based on MouseWheel events

--- examples/mouse_wheel.rs
+++ examples/mouse_wheel.rs
@@ -23,7 +23,26 @@ fn main() {
                 _ => (),
             },
             Event::DeviceEvent { event, .. } => match event {
-                DeviceEvent::MouseWheel { delta } => println!("mouse wheel: {:?}", delta),
+                DeviceEvent::MouseWheel { delta } => {
+                    match delta {
+                        winit::event::MouseScrollDelta::LineDelta(x,y) => {
+                            println!("mouse wheel Line Delta: ({},{})", x, y);
+                            let pixels_per_line = 120.0;
+                            let mut pos = window.outer_position().unwrap();
+                            pos.x -= (x * pixels_per_line) as i32;
+                            pos.y -= (y * pixels_per_line) as i32;
+                            window.set_outer_position(pos)
+                        }
+                        winit::event::MouseScrollDelta::PixelDelta(p) =>
+                        {
+                            println!("mouse wheel Pixel Delta: ({},{})", p.x, p.y);
+                            let mut pos = window.outer_position().unwrap();
+                            pos.x -= p.x as i32;
+                            pos.y -= p.y as i32;
+                            window.set_outer_position(pos)
+                        }
+                    }
+                }
                 _ => (),
             },
             _ => (),
