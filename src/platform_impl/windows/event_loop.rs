d38f142a339e2c55f8eeb6028fefb87a52c5d0b8
i509VCB <git@i509.me>
2021-05-28 11:28:45 -0500

    Reflow to be a strictly 120 columns or below

--- src/platform_impl/windows/event_loop.rs
+++ src/platform_impl/windows/event_loop.rs
@@ -267,17 +267,19 @@ impl<T> EventLoopWindowTarget<T> {
 
 /// Returns the id of the main thread.
 ///
-/// Windows has no real API to check if the current executing thread is the "main thread", unlike macOS.
+/// Windows has no real API to check if the current executing thread is the "main thread", unlike
+/// macOS.
 ///
-/// Windows will let us look up the current thread's id, but there's no API that lets us check what the
-/// id of the main thread is. We would somehow need to get the main thread's id before a developer
-/// could spin off any other threads inside of the main entrypoint in order to emulate the capabilities
-/// of other platforms.
+/// Windows will let us look up the current thread's id, but there's no API that lets us check what
+/// the id of the main thread is. We would somehow need to get the main thread's id before a
+/// developer could spin off any other threads inside of the main entrypoint in order to emulate the
+/// capabilities of other platforms.
 ///
 /// We can get the id of the main thread by using CRT initialization. CRT initialization can be used
-/// to setup global state within a program. The OS will call a list of function pointers which assign
-/// values to a static variable. To have get a hold of the main thread id, we need to place our
-/// function pointer inside of the `.CRT$XCU` section so it is called before the main entrypoint.
+/// to setup global state within a program. The OS will call a list of function pointers which
+/// assign values to a static variable. To have get a hold of the main thread id, we need to place
+/// our function pointer inside of the `.CRT$XCU` section so it is called before the main
+/// entrypoint.
 ///
 /// Full details of CRT initialization can be found here:
 /// https://docs.microsoft.com/en-us/cpp/c-runtime-library/crt-initialization?view=msvc-160
