b176f5d65396a1c86add95e70a6348330b37b965
t-sin <shinichi.tanaka45@gmail.com>
2021-12-15 09:34:05 +0900

    Use platform-specific parent ID when creating new X11 windows

--- src/platform_impl/linux/x11/window.rs
+++ src/platform_impl/linux/x11/window.rs
@@ -115,7 +115,11 @@ impl UnownedWindow {
         pl_attribs: PlatformSpecificWindowBuilderAttributes,
     ) -> Result<UnownedWindow, RootOsError> {
         let xconn = &event_loop.xconn;
-        let root = event_loop.root;
+        let root = if let Some(id) = pl_attribs.parent_id {
+            id
+        } else {
+            event_loop.root
+        };
 
         let mut monitors = xconn.available_monitors();
         let guessed_monitor = if monitors.is_empty() {
