c8b2041b7cc69f8b88437ad802ee89d2d61cd9c5
amrbashir <amr.bashir2015@gmail.com>
2022-03-29 12:25:14 +0200

    Revert "fix size overflow when window is undecorated"
    This reverts commit 4c0c7607f666eed32956bbde69e9fa2207d06724.

--- src/platform_impl/windows/event_loop/runner.rs
+++ src/platform_impl/windows/event_loop/runner.rs
@@ -436,7 +436,6 @@ impl<T> BufferedEvent<T> {
                     (window_id.0).0,
                     new_inner_size.width as _,
                     new_inner_size.height as _,
-                    false,
                 );
             }
         }
