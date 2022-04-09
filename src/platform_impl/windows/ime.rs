145c68c5fb6f233b1fa7d2df699580147ba98ef0
moko256 <koutaro.mo@gmail.com>
2022-04-10 01:09:21 +0900

    return cursor pos

--- src/platform_impl/windows/ime.rs
+++ src/platform_impl/windows/ime.rs
@@ -12,7 +12,8 @@ use windows_sys::Win32::{
         Input::Ime::{
             ImmAssociateContextEx, ImmGetCompositionStringW, ImmGetContext, ImmReleaseContext,
             ImmSetCandidateWindow, ATTR_TARGET_CONVERTED, ATTR_TARGET_NOTCONVERTED, CANDIDATEFORM,
-            CFS_EXCLUDE, GCS_COMPATTR, GCS_COMPSTR, GCS_RESULTSTR, IACE_CHILDREN, IACE_DEFAULT,
+            CFS_EXCLUDE, GCS_COMPATTR, GCS_COMPSTR, GCS_CURSORPOS, GCS_RESULTSTR, IACE_CHILDREN,
+            IACE_DEFAULT,
         },
         WindowsAndMessaging::{GetSystemMetrics, SM_IMMENABLED},
     },
@@ -55,14 +56,16 @@ impl ImeContext {
                 if first != None && last == None {
                     last = Some(text.len());
                 } else if first == None {
-                    // IME haven't split words and select any clause yet.
-                    first = None;
-                    last = None;
+                    // IME haven't split words and select any clause yet, so trying to retrieve normal cursor.
+                    let cursor = self.get_composition_cursor(&text);
+                    first = cursor;
+                    last = cursor;
                 }
 
                 Some((text, first, last))
             } else {
-                Some((text, None, None))
+                let cursor = self.get_composition_cursor(&text);
+                Some((text, cursor, cursor))
             }
         } else {
             None
@@ -71,6 +74,15 @@ impl ImeContext {
 
     pub unsafe fn get_composed_text(&self) -> Option<String> {
         self.get_composition_string(GCS_RESULTSTR)
+    }
+
+    unsafe fn get_composition_cursor(&self, text: &str) -> Option<usize> {
+        let cursor = ImmGetCompositionStringW(self.himc, GCS_CURSORPOS, null_mut(), 0);
+        if cursor >= 0 {
+            Some(text.chars().take(cursor as _).map(|c| c.len_utf8()).sum())
+        } else {
+            None
+        }
     }
 
     unsafe fn get_composition_string(&self, gcs_mode: u32) -> Option<String> {
