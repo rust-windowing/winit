6450e8b071c57df3c1db0ba1012f548918a3d4cd
Liam Murphy <liampm32@gmail.com>
2022-05-19 19:39:49 +1000

    Add a test that `EventLoopProxy` is `Sync`

--- tests/sync_object.rs
+++ tests/sync_object.rs
@@ -3,6 +3,16 @@ fn needs_sync<T: Sync>() {}
 
 #[cfg(not(target_arch = "wasm32"))]
 #[test]
+fn event_loop_proxy_sync() {
+    #[allow(dead_code)]
+    fn is_send<T: 'static + Send>() {
+        // ensures that `winit::EventLoopProxy` implements `Sync`
+        needs_sync::<winit::event_loop::EventLoopProxy<T>>();
+    }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+#[test]
 fn window_sync() {
     // ensures that `winit::Window` implements `Sync`
     needs_sync::<winit::window::Window>();
