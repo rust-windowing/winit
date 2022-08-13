b75fc13d7bf7e3bd618f11e18c85d641d84c6b18
Mads Marquart <mads@marquart.dk>
2022-08-13 15:35:40 +0200

    Simplify platform! macro

--- src/platform_impl/macros.rs
+++ src/platform_impl/macros.rs
@@ -45,21 +45,6 @@ macro_rules! __platform_parse_until_curly_bracket {
     };
 }
 
-macro_rules! __platform_parse_until_fat_arrow {
-    {($macro_out:ident) ($($prefix:tt)*) ($($item:tt)*) (=> $($body:tt)*)} => {
-        $macro_out! {
-            $($prefix)* {
-                ($($item)*) => $($body)*
-            }
-        }
-    };
-    {($macro_out:ident) ($($prefix:tt)*) ($($item:tt)*) ($t:tt $($rest:tt)*)} => {
-        __platform_parse_until_fat_arrow! {
-            ($macro_out) ($($prefix)*) ($($item)* $t) ($($rest)*)
-        }
-    };
-}
-
 macro_rules! __platform_enum_out {
     (
         $(#[$m:meta])* $v:vis enum ($($enum:tt)*) {
@@ -114,35 +99,18 @@ macro_rules! __platform_match_parse_inner {
     (
         $(use $import:ident::__Platform__;)?
         match ($item:expr) {
-            $($t:tt)*
-        }
-    ) => {
-        __platform_parse_until_fat_arrow! {
-            (__platform_match_out)
-            ($(use $import::__Platform__;)? match ($item))
-            ()
-            ($($t)*)
-        }
-    };
-}
-
-macro_rules! __platform_match_out {
-    (
-        $(use $import:ident::__Platform__;)?
-        match ($item:expr) {
-            ($($p:tt)*) => $x:expr
-            $(, _ => $fallback:expr)? $(,)?
+            $enum:ident::__Platform__$(($p:pat))? => $x:expr $(,)?
         }
     ) => {
         match $item {
             #[cfg(target_os = "ios")]
-            __platform_pattern_replace!((Ios) () ($($p)+)) => {
+            $enum::Ios$(($p))? => {
                 $(use $import::Ios as __Platform__;)?
                 $x
             }
 
             #[cfg(target_os = "macos")]
-            __platform_pattern_replace!((Macos) () ($($p)+)) => {
+            $enum::Macos$(($p))? => {
                 $(use $import::Macos as __Platform__;)?
                 $x
             }
@@ -157,7 +125,7 @@ macro_rules! __platform_match_out {
                     target_os = "openbsd",
                 ),
             ))]
-            __platform_pattern_replace!((X11) () ($($p)+)) => {
+            $enum::X11$(($p))? => {
                 $(use $import::X11 as __Platform__;)?
                 $x
             }
@@ -172,83 +140,117 @@ macro_rules! __platform_match_out {
                     target_os = "openbsd",
                 ),
             ))]
-            __platform_pattern_replace!((Wayland) () ($($p)+)) => {
+            $enum::Wayland$(($p))? => {
                 $(use $import::Wayland as __Platform__;)?
                 $x
             }
 
             #[cfg(target_os = "windows")]
-            __platform_pattern_replace!((Windows) () ($($p)+)) => {
+            $enum::Windows$(($p))? => {
                 $(use $import::Windows as __Platform__;)?
                 $x
             }
 
             #[cfg(target_arch = "wasm32")]
-            __platform_pattern_replace!((Web) () ($($p)+)) => {
+            $enum::Web$(($p))? => {
                 $(use $import::Web as __Platform__;)?
                 $x
             }
 
             #[cfg(target_os = "android")]
-            __platform_pattern_replace!((Android) () ($($p)+)) => {
+            $enum::Android$(($p))? => {
+                $(use $import::Android as __Platform__;)?
+                $x
+            }
+        }
+    };
+    // If no comma after expression
+    (
+        $(use $import:ident::__Platform__;)?
+        match ($item:expr) {
+            ($($enum:ident::__Platform__$(($p:pat))?),+ $(,)?) => { $($x:tt)* }
+            _ => $fallback:expr $(,)?
+        }
+    ) => {
+        __platform_match_parse_inner!(
+            $(use $import::__Platform__;)?
+            match ($item) {
+                ($($enum::__Platform__$(($p))?),+) => { $($x)* },
+                _ => $fallback,
+            }
+        )
+    };
+    // Usual case
+    (
+        $(use $import:ident::__Platform__;)?
+        match ($item:expr) {
+            ($($enum:ident::__Platform__$(($p:pat))?),+ $(,)?) => $x:expr,
+            _ => $fallback:expr $(,)?
+        }
+    ) => {
+        match $item {
+            #[cfg(target_os = "ios")]
+            ($($enum::Ios$(($p))?),+) => {
+                $(use $import::Ios as __Platform__;)?
+                $x
+            }
+
+            #[cfg(target_os = "macos")]
+            ($($enum::Macos$(($p))?),+) => {
+                $(use $import::Macos as __Platform__;)?
+                $x
+            }
+
+            #[cfg(all(
+                feature = "x11",
+                any(
+                    target_os = "linux",
+                    target_os = "dragonfly",
+                    target_os = "freebsd",
+                    target_os = "netbsd",
+                    target_os = "openbsd",
+                ),
+            ))]
+            ($($enum::X11$(($p))?),+) => {
+                $(use $import::X11 as __Platform__;)?
+                $x
+            }
+
+            #[cfg(all(
+                feature = "wayland",
+                any(
+                    target_os = "linux",
+                    target_os = "dragonfly",
+                    target_os = "freebsd",
+                    target_os = "netbsd",
+                    target_os = "openbsd",
+                ),
+            ))]
+            ($($enum::Wayland$(($p))?),+) => {
+                $(use $import::Wayland as __Platform__;)?
+                $x
+            }
+
+            #[cfg(target_os = "windows")]
+            ($($enum::Windows$(($p))?),+) => {
+                $(use $import::Windows as __Platform__;)?
+                $x
+            }
+
+            #[cfg(target_arch = "wasm32")]
+            ($($enum::Web$(($p))?),+) => {
+                $(use $import::Web as __Platform__;)?
+                $x
+            }
+
+            #[cfg(target_os = "android")]
+            ($($enum::Android$(($p))?),+) => {
                 $(use $import::Android as __Platform__;)?
                 $x
             }
 
-            $(
-                #[allow(unreachable_patterns)]
-                _ => $fallback
-            )?
-        }
-    };
-}
-
-/// Replace __Platform__ with the specified path
-macro_rules! __platform_pattern_replace {
-    {($($path:tt)*) ($($output:tt)*) (__Platform__ $($rest:tt)*)} => {
-        __platform_pattern_replace! {
-            ($($path)*)
-            ($($output)* $($path)*)
-            ($($rest)*)
-        }
-    };
-    // Recurse into parentheses
-    {($($path:tt)*) ($($output:tt)*) (( $($rest:tt)* ))} => {
-        __platform_pattern_replace_in_group! {
-            ($($path)*)
-            ($($output)*)
-            ()
-            ()
-            ($($rest)*)
-        }
-    };
-    {($($path:tt)*) ($($output:tt)*) ($t:tt $($rest:tt)*)} => {
-        __platform_pattern_replace! {
-            ($($path)*)
-            ($($output)* $t)
-            ($($rest)*)
-        }
-    };
-    {($($path:tt)*) ($($output:tt)*) ()} => {
-        $($output)*
-    };
-}
-
-macro_rules! __platform_pattern_replace_in_group {
-    {($($path:tt)*) ($($output:tt)*) ($($parsed:tt)*) ($($item:tt)*) ($(,)?)} => {
-        $($output)* (
-            $($parsed)*
-            __platform_pattern_replace!(($($path)*) () ($($item)*))
-        )
-    };
-    {($($path:tt)*) ($($output:tt)*) ($($parsed:tt)*) ($($item:tt)*) (, $($rest:tt)*)} => {
-        __platform_pattern_replace_in_group! {
-            ($($path)*) ($($output)*) ($($parsed)* __platform_pattern_replace!(($($path)*) () ($($item)*)), ) () ($($rest)*)
-        }
-    };
-    {($($path:tt)*) ($($output:tt)*) ($($parsed:tt)*) ($($item:tt)*) ($t:tt $($rest:tt)*)} => {
-        __platform_pattern_replace_in_group! {
-            ($($path)*) ($($output)*) ($($parsed)*) ($($item)* $t) ($($rest)*)
+            #[allow(unreachable_patterns)]
+            _ => $fallback
         }
     };
 }
