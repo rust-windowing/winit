fa965cd7316b9bba78e1ed16c65adfa37e1bd099
amrbashir <amr.bashir2015@gmail.com>
2022-11-01 19:30:09 +0200

    use util::decode_wide

--- src/platform_impl/windows/window.rs
+++ src/platform_impl/windows/window.rs
@@ -700,10 +700,10 @@ impl Window {
 
     #[inline]
     pub fn title(&self) -> String {
-        let len = unsafe { GetWindowTextLengthW(self.window.0) };
-        let mut buf = vec![0; (len + 1) as usize];
+        let len = unsafe { GetWindowTextLengthW(self.window.0) } + 1;
+        let mut buf = vec![0; len as usize];
         unsafe { GetWindowTextW(self.window.0, buf.as_mut_ptr(), len) };
-        String::from_utf16_lossy(&buf[..len as _])
+        util::decode_wide(&buf).to_string_lossy().to_string()
     }
 
     #[inline]
