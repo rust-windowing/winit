b8fac9c7c2b518657024e258cde4f9db703cd103
amrbashir <amr.bashir2015@gmail.com>
2022-11-23 15:16:08 +0200

    add macOS notes

--- src/window.rs
+++ src/window.rs
@@ -345,6 +345,7 @@ impl WindowBuilder {
     ///
     /// ## Platform-specific
     ///
+    /// - **macOS:** This is an app-wide setting.
     /// - **Wayland:** This control only CSD. You can also use `WINIT_WAYLAND_CSD_THEME` env variable to set the theme.
     ///   Possible values for env variable are: "dark" and light".
     /// - **x11:** Build window with `_GTK_THEME_VARIANT` hint set to `dark` or `light`.
@@ -948,6 +949,7 @@ impl Window {
     ///
     /// ## Platform-specific
     ///
+    /// - **macOS:** This is an app-wide setting.
     /// - **Wayland:** You can also use `WINIT_WAYLAND_CSD_THEME` env variable to set the theme.
     /// Possible values for env variable are: "dark" and light". When unspecified, a theme is automatically selected.
     /// -**x11:** Sets `_GTK_THEME_VARIANT` hint to `dark` or `light`.
@@ -962,6 +964,7 @@ impl Window {
     ///
     /// ## Platform-specific
     ///
+    /// - **macOS:** This is an app-wide setting.
     /// - **iOS / Android / Web / Wayland / x11:** Unsupported.
     #[inline]
     pub fn theme(&self) -> Option<Theme> {
