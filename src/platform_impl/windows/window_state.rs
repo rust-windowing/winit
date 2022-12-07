fce3aad49ce05d075764629f820f038141983fd1
amrbashir <amr.bashir2015@gmail.com>
2022-12-07 22:37:11 +0200

    just remove the `MINIMIZED` flag
    Co-authored-by: Markus Siglreithmaier <m.siglreith@gmail.com>

--- src/platform_impl/windows/window_state.rs
+++ src/platform_impl/windows/window_state.rs
@@ -114,12 +114,6 @@ bitflags! {
         /// Drop shadow for undecorated windows.
         const MARKER_UNDECORATED_SHADOW = 1 << 20;
 
-        /// When minmizing a maximized Windows, `WM_SIZE` which we use to update the `MAXIMIZED` bit
-        /// is fired with `wparam` set to `SIZE_MINIMIZED` and thus the `MAXIMIZED` bit will be unset
-        /// and when un-minimizing the window, the `MAXIMIZED` bit will still be unset and later on
-        /// in `apply_diff` when `new.to_window_styles()` is called, it will not add `WS_MAXIMIZE` window style.
-        const MARKER_WAS_MAXIMIZED = 1 << 21;
-
         const EXCLUSIVE_FULLSCREEN_OR_MASK = WindowFlags::ALWAYS_ON_TOP.bits;
     }
 }
@@ -301,7 +295,7 @@ impl WindowFlags {
         self = self.mask();
         new = new.mask();
 
-        let diff = self ^ new;
+        let mut diff = self ^ new;
 
         if diff == WindowFlags::empty() {
             return;
@@ -359,6 +353,8 @@ impl WindowFlags {
                     },
                 );
             }
+
+            diff.remove(WindowFlags::MINIMIZED);
         }
 
         if diff.contains(WindowFlags::CLOSABLE) || new.contains(WindowFlags::CLOSABLE) {
@@ -380,17 +376,13 @@ impl WindowFlags {
         }
 
         if diff != WindowFlags::empty() {
-            let (mut style, style_ex) = new.to_window_styles();
+            let (style, style_ex) = new.to_window_styles();
 
             unsafe {
                 SendMessageW(window, *event_loop::SET_RETAIN_STATE_ON_SIZE_MSG_ID, 1, 0);
 
                 // This condition is necessary to avoid having an unrestorable window
                 if !new.contains(WindowFlags::MINIMIZED) {
-                    if self.contains(WindowFlags::MARKER_WAS_MAXIMIZED) {
-                        style |= WS_MAXIMIZE;
-                    }
-
                     SetWindowLongW(window, GWL_STYLE, style as i32);
                     SetWindowLongW(window, GWL_EXSTYLE, style_ex as i32);
                 }
