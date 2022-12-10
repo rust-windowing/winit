48de6938dd1d26fbdf009d8fd274803b01eacd30
Amr Bashir <amr.bashir2015@gmail.com>
2022-12-11 00:16:23 +0200

    Update build.rs
    Co-authored-by: Mads Marquart <mads@marquart.dk>

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 use cfg_aliases::cfg_aliases;
 
 fn main() {
+    // The script doesn't depend on our code
+    println!("cargo:rerun-if-changed=build.rs");
     // Setup cfg aliases
     cfg_aliases! {
         // Platforms
