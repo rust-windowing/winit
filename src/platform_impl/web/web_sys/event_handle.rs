53e180782029bce0cffabb54313dc4fc8e2760c5
Douglas Dwyer <dougmdwyer@hotmail.com>
2023-01-13 21:26:29 -0500

    Fix formatting

--- src/platform_impl/web/web_sys/event_handle.rs
+++ src/platform_impl/web/web_sys/event_handle.rs
@@ -5,7 +5,7 @@ pub(super) struct EventListenerHandle<T: ?Sized> {
     target: EventTarget,
     event_type: &'static str,
     listener: Closure<T>,
-    options: EventListenerOptions
+    options: EventListenerOptions,
 }
 
 impl<T: ?Sized> EventListenerHandle<T> {
@@ -21,7 +21,7 @@ impl<T: ?Sized> EventListenerHandle<T> {
             target,
             event_type,
             listener,
-            options: EventListenerOptions::new()
+            options: EventListenerOptions::new(),
         }
     }
 
@@ -46,7 +46,7 @@ impl<T: ?Sized> EventListenerHandle<T> {
             target,
             event_type,
             listener,
-            options: options.clone().unchecked_into()
+            options: options.clone().unchecked_into(),
         }
     }
 }
@@ -57,7 +57,7 @@ impl<T: ?Sized> Drop for EventListenerHandle<T> {
             .remove_event_listener_with_callback_and_event_listener_options(
                 self.event_type,
                 self.listener.as_ref().unchecked_ref(),
-                &self.options
+                &self.options,
             )
             .unwrap_or_else(|e| {
                 web_sys::console::error_2(
