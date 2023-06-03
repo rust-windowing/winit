rust-windowing/winit

commit 31ebc5caf4ac43fe3bb334f66e715e5dfd95bea6

Update `bitflags` to `2.0`
Co-authored-by: dAxpeDDa <daxpedda@gmail.com>

src/keyboard.rs
@@ -1572,7 +1572,7 @@ bitflags! {
     /// Represents the current state of the keyboard modifiers
     ///
     /// Each flag represents a modifier and is set if this modifier is active.
-    #[derive(Default)]
+    #[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
     pub struct ModifiersState: u32 {
         /// The "shift" key.
         const SHIFT = 0b100;
@@ -1623,7 +1623,7 @@ pub enum ModifiersKeyState {
 // to treat modifiers differently based on their position, which is required
 // on macOS due to their AltGr/Option situation.
 bitflags! {
-    #[derive(Default)]
+    #[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
     pub(crate) struct ModifiersKeys: u8 {
         const LSHIFT   = 0b0000_0001;
         const RSHIFT   = 0b0000_0010;

src/platform/ios.rs
@@ -287,14 +287,14 @@ bitflags! {
     /// The [edges] of a screen.
     ///
     /// [edges]: https://developer.apple.com/documentation/uikit/uirectedge?language=objc
-    #[derive(Default)]
+    #[derive(Default, Clone, Copy)]
     pub struct ScreenEdge: u8 {
         const NONE   = 0;
         const TOP    = 1 << 0;
         const LEFT   = 1 << 1;
         const BOTTOM = 1 << 2;
         const RIGHT  = 1 << 3;
-        const ALL = ScreenEdge::TOP.bits | ScreenEdge::LEFT.bits
-            | ScreenEdge::BOTTOM.bits | ScreenEdge::RIGHT.bits;
+        const ALL = ScreenEdge::TOP.bits() | ScreenEdge::LEFT.bits()
+            | ScreenEdge::BOTTOM.bits() | ScreenEdge::RIGHT.bits();
     }
 }

Cargo.toml
@@ -50,7 +50,7 @@ serde = ["dep:serde", "cursor-icon/serde", "smol_str/serde"]
 cfg_aliases = "0.1.1"
 
 [dependencies]
-bitflags = "1"
+bitflags = "2"
 cursor-icon = "1.0.0"
 instant = { version = "0.1", features = ["wasm-bindgen"] }
 log = "0.4"

src/platform_impl/ios/uikit/view_controller.rs
@@ -39,6 +39,7 @@ extern_methods!(
 );
 
 bitflags! {
+    #[derive(Clone, Copy)]
     pub struct UIInterfaceOrientationMask: NSUInteger {
         const Portrait = 1 << 1;
         const PortraitUpsideDown = 1 << 2;

src/platform_impl/macos/appkit/application.rs
@@ -109,6 +109,7 @@ unsafe impl Encode for NSApplicationActivationPolicy {
 }
 
 bitflags! {
+    #[derive(Debug, Clone, Copy)]
     pub struct NSApplicationPresentationOptions: NSUInteger {
         const NSApplicationPresentationDefault = 0;
         const NSApplicationPresentationAutoHideDock = 1 << 0;

src/platform_impl/macos/appkit/event.rs
@@ -194,6 +194,7 @@ const NX_DEVICERALTKEYMASK: u32 = 0x00000040;
 const NX_DEVICERCTLKEYMASK: u32 = 0x00002000;
 
 bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
     pub struct NSEventModifierFlags: NSUInteger {
         const NSAlphaShiftKeyMask                     = 1 << 16;
         const NSShiftKeyMask                          = 1 << 17;
@@ -212,6 +213,7 @@ unsafe impl Encode for NSEventModifierFlags {
 }
 
 bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
     pub struct NSEventPhase: NSUInteger {
        const NSEventPhaseNone        = 0;
        const NSEventPhaseBegan       = 0x1 << 0;

src/platform_impl/macos/appkit/window.rs
@@ -336,6 +336,7 @@ unsafe impl Encode for NSWindowLevel {
 }
 
 bitflags! {
+    #[derive(Clone, Copy)]
     pub struct NSWindowOcclusionState: NSUInteger {
         const NSWindowOcclusionStateVisible = 1 << 1;
     }
@@ -346,6 +347,7 @@ unsafe impl Encode for NSWindowOcclusionState {
 }
 
 bitflags! {
+    #[derive(Debug, Clone, Copy)]
     pub struct NSWindowStyleMask: NSUInteger {
         const NSBorderlessWindowMask      = 0;
         const NSTitledWindowMask          = 1 << 0;

src/platform_impl/macos/view.rs
@@ -71,6 +71,7 @@ enum ImeState {
 }
 
 bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq)]
     struct ModLocationMask: u8 {
         const LEFT = 1;
         const RIGHT = 2;

commit 8fc88a02c67388bed86716c863b84376a5da8982

Add missing `Hash` impls on bitflags
Some bitflags in public API lost their `Hash` implementations.

Fixes: 31ebc5caf (Update `bitflags` to `2.0`)

src/keyboard.rs
@@ -1572,7 +1572,7 @@ bitflags! {
     /// Represents the current state of the keyboard modifiers
     ///
     /// Each flag represents a modifier and is set if this modifier is active.
-    #[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
+    #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
     pub struct ModifiersState: u32 {
         /// The "shift" key.
         const SHIFT = 0b100;
@@ -1623,7 +1623,7 @@ pub enum ModifiersKeyState {
 // to treat modifiers differently based on their position, which is required
 // on macOS due to their AltGr/Option situation.
 bitflags! {
-    #[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
+    #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
     pub(crate) struct ModifiersKeys: u8 {
         const LSHIFT   = 0b0000_0001;
         const RSHIFT   = 0b0000_0010;

src/platform/ios.rs
@@ -253,7 +253,7 @@ impl MonitorHandleExtIOS for MonitorHandle {
 }
 
 /// Valid orientations for a particular [`Window`].
-#[derive(Clone, Copy, Debug, Default)]
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum ValidOrientations {
     /// Excludes `PortraitUpsideDown` on iphone
     #[default]
@@ -268,7 +268,7 @@ pub enum ValidOrientations {
 /// The device [idiom].
 ///
 /// [idiom]: https://developer.apple.com/documentation/uikit/uidevice/1620037-userinterfaceidiom?language=objc
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Idiom {
     Unspecified,
 
@@ -287,7 +287,7 @@ bitflags! {
     /// The [edges] of a screen.
     ///
     /// [edges]: https://developer.apple.com/documentation/uikit/uirectedge?language=objc
-    #[derive(Default, Clone, Copy)]
+    #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
     pub struct ScreenEdge: u8 {
         const NONE   = 0;
         const TOP    = 1 << 0;