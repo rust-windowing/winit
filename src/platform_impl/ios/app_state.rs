7de2bc7ae6eec254e2423f3f59e8ae78338b7767
François <mockersf@gmail.com>
2023-07-28 17:52:24 +0200

    iOS: Always set timer when polling to avoid slow waking (#2979)
    * Always set timer when polling to avoid slow waking
    
    * add comment and changelog
    
    ---------
    
    Co-authored-by: Dusty DeWeese <dustin.deweese@gmail.com>
    Co-authored-by: Mads Marquart <mads@marquart.dk>

--- src/platform_impl/ios/app_state.rs
+++ src/platform_impl/ios/app_state.rs
@@ -393,9 +393,6 @@ impl AppState {
 
         let new = self.control_flow;
         match (old, new) {
-            (ControlFlow::Poll, ControlFlow::Poll) => self.set_state(AppStateImpl::PollFinished {
-                waiting_event_handler,
-            }),
             (ControlFlow::Wait, ControlFlow::Wait) => {
                 let start = Instant::now();
                 self.set_state(AppStateImpl::Waiting {
@@ -428,6 +425,7 @@ impl AppState {
                 });
                 self.waker.start_at(new_instant)
             }
+            // Unlike on macOS, handle Poll to Poll transition here to call the waker
             (_, ControlFlow::Poll) => {
                 self.set_state(AppStateImpl::PollFinished {
                     waiting_event_handler,
