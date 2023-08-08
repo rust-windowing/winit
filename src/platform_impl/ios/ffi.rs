d79a6f80faa5a246aed766c489e91d4b6b13bdaf
Fredrik Fornwall <fredrik@fornwall.net>
2023-08-08 23:28:11 +0200

    iOS: Use NSOperatingSystemVersion from icrate

--- src/platform_impl/ios/ffi.rs
+++ src/platform_impl/ios/ffi.rs
@@ -7,25 +7,6 @@ use objc2::encode::{Encode, Encoding};
 
 use crate::platform::ios::{Idiom, ScreenEdge};
 
-#[repr(C)]
-#[derive(Clone, Debug)]
-pub struct NSOperatingSystemVersion {
-    pub major: NSInteger,
-    pub minor: NSInteger,
-    pub patch: NSInteger,
-}
-
-unsafe impl Encode for NSOperatingSystemVersion {
-    const ENCODING: Encoding = Encoding::Struct(
-        "NSOperatingSystemVersion",
-        &[
-            NSInteger::ENCODING,
-            NSInteger::ENCODING,
-            NSInteger::ENCODING,
-        ],
-    );
-}
-
 #[repr(transparent)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct UIUserInterfaceIdiom(NSInteger);
