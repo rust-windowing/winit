8de232fd27977026efc93b86e33b351a5e4a4b57
daxpedda <daxpedda@gmail.com>
2023-09-03 02:26:08 +0200

    Reimplement `Waker` type without channels

--- src/platform_impl/web/async/waker.rs
+++ src/platform_impl/web/async/waker.rs
@@ -1,41 +1,122 @@
-use super::AsyncSender;
-use super::{channel, Wrapper};
+use super::Wrapper;
+use atomic_waker::AtomicWaker;
+use std::future;
+use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+use std::sync::Arc;
+use std::task::Poll;
 
-type WakerValue<T, E> = Wrapper<false, (T, fn(&T, E)), AsyncSender<E>, E>;
+pub struct Waker<T: 'static> {
+    wrapper: Wrapper<false, Handler<T>, Sender, usize>,
+    counter: Arc<AtomicUsize>,
+    waker: Arc<AtomicWaker>,
+}
 
-pub struct Waker<T: 'static, E: 'static + Send>(WakerValue<T, E>);
+struct Handler<T> {
+    value: T,
+    handler: fn(&T, usize),
+}
 
-impl<T, E: Send> Waker<T, E> {
-    pub fn new(value: T, handler: fn(&T, E)) -> Option<Self> {
-        let (sender, receiver) = channel::<E>();
+#[derive(Clone)]
+struct Sender {
+    counter: Arc<AtomicUsize>,
+    waker: Arc<AtomicWaker>,
+    closed: Arc<AtomicBool>,
+}
 
-        Wrapper::new(
-            (value, handler),
-            |lock, event| {
-                let lock = lock.read().unwrap();
-                let (value, handler) = lock.as_ref().unwrap();
-                handler(value, event)
-            },
-            move |lock| async move {
-                while let Ok(event) = receiver.next().await {
-                    let lock = lock.read().unwrap();
-                    let (value, handler) = lock.as_ref().unwrap();
-                    handler(value, event)
-                }
-            },
-            sender,
-            |sender, event| sender.send(event).unwrap(),
-        )
-        .map(Self)
-    }
-
-    pub fn send(&self, event: E) {
-        self.0.send(event)
+impl Drop for Sender {
+    fn drop(&mut self) {
+        if Arc::strong_count(&self.closed) == 1 {
+            self.closed.store(true, Ordering::Relaxed);
+            self.waker.wake();
+        }
     }
 }
 
-impl<T, E: Send> Clone for Waker<T, E> {
+impl<T> Waker<T> {
+    #[track_caller]
+    pub fn new(value: T, handler: fn(&T, usize)) -> Option<Self> {
+        let counter = Arc::new(AtomicUsize::new(0));
+        let waker = Arc::new(AtomicWaker::new());
+        let closed = Arc::new(AtomicBool::new(false));
+
+        let handler = Handler { value, handler };
+
+        let sender = Sender {
+            counter: Arc::clone(&counter),
+            waker: Arc::clone(&waker),
+            closed: Arc::clone(&closed),
+        };
+
+        let wrapper = Wrapper::new(
+            handler,
+            |handler, count| {
+                let handler = handler.read().unwrap();
+                let handler = handler.as_ref().unwrap();
+                (handler.handler)(&handler.value, count);
+            },
+            {
+                let counter = Arc::clone(&counter);
+                let waker = Arc::clone(&waker);
+
+                move |handler| async move {
+                    while let Some(count) = future::poll_fn(|cx| {
+                        let count = counter.swap(0, Ordering::Relaxed);
+
+                        if count > 0 {
+                            Poll::Ready(Some(count))
+                        } else {
+                            if closed.load(Ordering::Relaxed) {
+                                return Poll::Ready(None);
+                            }
+
+                            waker.register(cx.waker());
+
+                            let count = counter.swap(0, Ordering::Relaxed);
+
+                            if count > 0 {
+                                Poll::Ready(Some(count))
+                            } else {
+                                if closed.load(Ordering::Relaxed) {
+                                    return Poll::Ready(None);
+                                }
+
+                                Poll::Pending
+                            }
+                        }
+                    })
+                    .await
+                    {
+                        let handler = handler.read().unwrap();
+                        let handler = handler.as_ref().unwrap();
+                        (handler.handler)(&handler.value, count);
+                    }
+                }
+            },
+            sender,
+            |inner, _| {
+                inner.counter.fetch_add(1, Ordering::Relaxed);
+                inner.waker.wake();
+            },
+        )?;
+
+        Some(Self {
+            wrapper,
+            counter,
+            waker,
+        })
+    }
+
+    pub fn wake(&self) {
+        self.wrapper.send(1)
+    }
+}
+
+impl<T> Clone for Waker<T> {
     fn clone(&self) -> Self {
-        Self(self.0.clone())
+        Self {
+            wrapper: self.wrapper.clone(),
+            counter: Arc::clone(&self.counter),
+            waker: Arc::clone(&self.waker),
+        }
     }
 }
--- src/platform_impl/web/event_loop/proxy.rs
+++ src/platform_impl/web/event_loop/proxy.rs
@@ -1,3 +1,4 @@
+use std::iter;
 use std::sync::mpsc::Sender;
 
 use super::runner;
@@ -6,16 +7,15 @@ use crate::event_loop::EventLoopClosed;
 use crate::platform_impl::platform::r#async::Waker;
 
 pub struct EventLoopProxy<T: 'static> {
-    // used to wake the event loop handler, not to actually pass data
-    runner: Waker<runner::Shared, ()>,
+    runner: Waker<runner::Shared>,
     sender: Sender<T>,
 }
 
 impl<T: 'static> EventLoopProxy<T> {
     pub fn new(runner: runner::Shared, sender: Sender<T>) -> Self {
         Self {
-            runner: Waker::new(runner, |runner, event| {
-                runner.send_event(Event::UserEvent(event))
+            runner: Waker::new(runner, |runner, count| {
+                runner.send_events(iter::repeat(Event::UserEvent(())).take(count))
             })
             .unwrap(),
             sender,
@@ -24,7 +24,7 @@ impl<T: 'static> EventLoopProxy<T> {
 
     pub fn send_event(&self, event: T) -> Result<(), EventLoopClosed<T>> {
         self.sender.send(event).unwrap();
-        self.runner.send(());
+        self.runner.wake();
         Ok(())
     }
 }
