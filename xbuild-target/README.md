f9a975e6e519c070e6d7f538b1d1c90c5c66c0d8
John Nunley <dev@notgull.net>
2023-09-04 13:36:12 -0700

    Use xbuild instead of cargo apk in the CI
    In addition to cargo apk being deprecated, xbuild also has support for
    ios.
    
    Signed-off-by: John Nunley <dev@notgull.net>

---
+++ xbuild-target/README.md
@@ -0,0 +1,4 @@
+# xbuild-target
+
+This is a crate that can be built via `x build` in order to simplify CI testing.
+
---
+++ xbuild-target/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    winit::event_loop::EventLoop::new()
+        .unwrap()
+        .run(|_, _, _| todo!())
+        .ok();
+}
