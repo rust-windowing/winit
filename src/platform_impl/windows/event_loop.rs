rust-windowing/winit

commit d37d1a03b20004cf014f8a9bd62b2545419603ad

On Windows, fix deadlock during `Cursor{Enter,Leave}`
The lock was not released when calling back to the user.

Fixes #3171.

src/platform_impl/windows/event_loop.rs
@@ -1465,6 +1465,7 @@ unsafe fn public_window_callback_inner<T: 'static>(
                             .set_cursor_flags(window, |f| f.set(CursorFlags::IN_WINDOW, true))
                             .ok();
 
+                        drop(w);
                         userdata.send_event(Event::WindowEvent {
                             window_id: RootWindowId(WindowId(window)),
                             event: CursorEntered {
@@ -1487,6 +1488,7 @@ unsafe fn public_window_callback_inner<T: 'static>(
                             .set_cursor_flags(window, |f| f.set(CursorFlags::IN_WINDOW, false))
                             .ok();
 
+                        drop(w);
                         userdata.send_event(Event::WindowEvent {
                             window_id: RootWindowId(WindowId(window)),
                             event: CursorLeft {
@@ -1500,6 +1502,7 @@ unsafe fn public_window_callback_inner<T: 'static>(
                 // handle spurious WM_MOUSEMOVE messages
                 // see https://devblogs.microsoft.com/oldnewthing/20031001-00/?p=42343
                 // and http://debugandconquer.blogspot.com/2015/08/the-cause-of-spurious-mouse-move.html
+                let mut w = userdata.window_state_lock();
                 cursor_moved = w.mouse.last_position != Some(position);
                 w.mouse.last_position = Some(position);
             }

commit a75b5995f1e213599bb1d46436b7d848e3a2fb86

On Winows, Fix deedlock with `WM_MOUSEMOVE`
The lock was still present in `None` path.

Fixes: d37d1a03b(On Windows, fix deadlock during `Cursor{Enter,Leave}`)

src/platform_impl/windows/event_loop.rs
@@ -1496,7 +1496,7 @@ unsafe fn public_window_callback_inner<T: 'static>(
                             },
                         });
                     }
-                    PointerMoveKind::None => (),
+                    PointerMoveKind::None => drop(w),
                 }
 
                 // handle spurious WM_MOUSEMOVE messages