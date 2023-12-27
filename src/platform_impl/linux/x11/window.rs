23b3c127fdac724fbdfafef314512e1d4949f287
John Nunley <dev@notgull.net>
2023-12-26 21:25:27 -0800

    bugfix: Change value sent to X server during minimize
    Closes #3327
    
    Signed-off-by: John Nunley <dev@notgull.net>

--- src/platform_impl/linux/x11/window.rs
+++ src/platform_impl/linux/x11/window.rs
@@ -9,7 +9,7 @@ use std::{
 
 use x11rb::{
     connection::Connection,
-    properties::{WmHints, WmHintsState, WmSizeHints, WmSizeHintsSpecification},
+    properties::{WmHints, WmSizeHints, WmSizeHintsSpecification},
     protocol::{
         randr,
         shape::SK,
@@ -987,7 +987,7 @@ impl UnownedWindow {
                     xproto::EventMask::SUBSTRUCTURE_REDIRECT
                         | xproto::EventMask::SUBSTRUCTURE_NOTIFY,
                 ),
-                [WmHintsState::Iconic as u32, 0, 0, 0, 0],
+                [3u32, 0, 0, 0, 0],
             )
         } else {
             self.xconn.send_client_msg(
