6b507429148653ff16d3cb63cb8034c936e936c7
François <mockersf@gmail.com>
2024-01-15 21:39:19 +0100

    use UIView

--- src/platform_impl/ios/uikit/gesture_recognizer.rs
+++ src/platform_impl/ios/uikit/gesture_recognizer.rs
@@ -169,7 +169,6 @@ unsafe impl Encode for UIPinchGestureRecognizer {
     const ENCODING: Encoding = Encoding::Object;
 }
 
-// https://developer.apple.com/documentation/uikit/uirotationgesturerecognizer
 extern_class!(
     #[derive(Debug, PartialEq, Eq, Hash)]
     pub(crate) struct UIRotationGestureRecognizer;
