bed0033cd692d285904260e38ce4b1fea0be461b
james7132 <contact@jamessliu.com>
2024-02-24 15:54:10 -0800

    Don't allow closures

--- src/utils.rs
+++ src/utils.rs
@@ -2,31 +2,26 @@
 // Replace with std::sync::LazyLock when https://github.com/rust-lang/rust/issues/109736 is stablized.
 
 use std::ops::Deref;
-use std::sync::Mutex;
 use std::sync::OnceLock;
 
-pub(crate) struct Lazy<T, F = fn() -> T> {
+pub(crate) struct Lazy<T> {
     cell: OnceLock<T>,
-    init: Mutex<Option<F>>,
+    init: fn() -> T,
 }
 
-impl<T, F: FnOnce() -> T> Lazy<T, F> {
-    pub const fn new(f: F) -> Self {
+impl<T> Lazy<T> {
+    pub const fn new(f: fn() -> T) -> Self {
         Self {
             cell: OnceLock::new(),
-            init: Mutex::new(Some(f)),
+            init: f,
         }
     }
 }
 
-impl<T, F: FnOnce() -> T> Deref for Lazy<T, F> {
+impl<T> Deref for Lazy<T> {
     type Target = T;
     #[inline]
     fn deref(&self) -> &'_ T {
-        self.cell
-            .get_or_init(|| match self.init.lock().unwrap().take() {
-                Some(f) => f(),
-                None => unreachable!(),
-            })
+        self.cell.get_or_init(self.init)
     }
 }
