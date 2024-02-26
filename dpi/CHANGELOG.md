bdeeaa290d480b904a47a74655e5d78893ac9808
amrbashir <amr.bashir2015@gmail.com>
2024-02-27 01:28:41 +0200

    dpi: add `LogicalUnit`, `PhysicalUnit`, and `Unit`
    Part-off: https://github.com/rust-windowing/winit/issues/2395

---
+++ dpi/CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+Please keep one empty line before and after all headers. (This is required for
+`git` to produce a conflict when a release is made while a PR is open and the
+PR's changelog entry would go into the wrong section).
+
+And please only add new entries to the top of this list, right below the `#
+Unreleased` header.
+
+# Unreleased
+
+- Add `LogicalUnit`, `PhysicalUnit` and `PixelUnit` types and related functions.
--- dpi/src/lib.rs
+++ dpi/src/lib.rs
@@ -35,8 +35,8 @@
 //!
 //! ### Position and Size types
 //!
-//! The [`PhysicalPosition`] / [`PhysicalSize`] types correspond with the actual pixels on the
-//! device, and the [`LogicalPosition`] / [`LogicalSize`] types correspond to the physical pixels
+//! The [`PhysicalPosition`] / [`PhysicalSize`] / [`PhysicalUnit`] types correspond with the actual pixels on the
+//! device, and the [`LogicalPosition`] / [`LogicalSize`] / [`LogicalUnit`] types correspond to the physical pixels
 //! divided by the scale factor.
 //!
 //! The position and size types are generic over their exact pixel type, `P`, to allow the
@@ -126,6 +126,238 @@ impl Pixel for f64 {
 #[inline]
 pub fn validate_scale_factor(scale_factor: f64) -> bool {
     scale_factor.is_sign_positive() && scale_factor.is_normal()
+}
+
+/// A logical pixel unit.
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Default, Hash)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct LogicalUnit<P>(pub P);
+
+impl<P> LogicalUnit<P> {
+    /// Represents a minimum logical unit of [`f64::MAX`].
+    pub const MIN: LogicalUnit<f64> = LogicalUnit::new(f64::MIN);
+    /// Represents a logical unit of `0_f64`.
+    pub const ZERO: LogicalUnit<f64> = LogicalUnit::new(0.0);
+    /// Represents a maximum logical unit that is equal to [`f64::MAX`].
+    pub const MAX: LogicalUnit<f64> = LogicalUnit::new(f64::MAX);
+
+    #[inline]
+    pub const fn new(v: P) -> Self {
+        LogicalUnit(v)
+    }
+}
+
+impl<P: Pixel> LogicalUnit<P> {
+    #[inline]
+    pub fn from_physical<T: Into<PhysicalUnit<X>>, X: Pixel>(
+        physical: T,
+        scale_factor: f64,
+    ) -> Self {
+        physical.into().to_logical(scale_factor)
+    }
+
+    #[inline]
+    pub fn to_physical<X: Pixel>(&self, scale_factor: f64) -> PhysicalUnit<X> {
+        assert!(validate_scale_factor(scale_factor));
+        PhysicalUnit::new(self.0.into() * scale_factor).cast()
+    }
+
+    #[inline]
+    pub fn cast<X: Pixel>(&self) -> LogicalUnit<X> {
+        LogicalUnit(self.0.cast())
+    }
+}
+
+impl<P: Pixel, X: Pixel> From<X> for LogicalUnit<P> {
+    fn from(v: X) -> LogicalUnit<P> {
+        LogicalUnit::new(v.cast())
+    }
+}
+
+impl<P: Pixel> From<LogicalUnit<P>> for u8 {
+    fn from(v: LogicalUnit<P>) -> u8 {
+        v.0.cast()
+    }
+}
+
+impl<P: Pixel> From<LogicalUnit<P>> for u16 {
+    fn from(v: LogicalUnit<P>) -> u16 {
+        v.0.cast()
+    }
+}
+
+impl<P: Pixel> From<LogicalUnit<P>> for u32 {
+    fn from(v: LogicalUnit<P>) -> u32 {
+        v.0.cast()
+    }
+}
+
+impl<P: Pixel> From<LogicalUnit<P>> for i8 {
+    fn from(v: LogicalUnit<P>) -> i8 {
+        v.0.cast()
+    }
+}
+
+impl<P: Pixel> From<LogicalUnit<P>> for i16 {
+    fn from(v: LogicalUnit<P>) -> i16 {
+        v.0.cast()
+    }
+}
+
+impl<P: Pixel> From<LogicalUnit<P>> for i32 {
+    fn from(v: LogicalUnit<P>) -> i32 {
+        v.0.cast()
+    }
+}
+
+impl<P: Pixel> From<LogicalUnit<P>> for f32 {
+    fn from(v: LogicalUnit<P>) -> f32 {
+        v.0.cast()
+    }
+}
+
+impl<P: Pixel> From<LogicalUnit<P>> for f64 {
+    fn from(v: LogicalUnit<P>) -> f64 {
+        v.0.cast()
+    }
+}
+
+/// A physical pixel unit.
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Default, Hash)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct PhysicalUnit<P>(pub P);
+
+impl<P> PhysicalUnit<P> {
+    /// Represents a minimum physical unit of [`f64::MAX`].
+    pub const MIN: LogicalUnit<f64> = LogicalUnit::new(f64::MIN);
+    /// Represents a physical unit of `0_f64`.
+    pub const ZERO: LogicalUnit<f64> = LogicalUnit::new(0.0);
+    /// Represents a maximum physical unit that is equal to [`f64::MAX`].
+    pub const MAX: LogicalUnit<f64> = LogicalUnit::new(f64::MAX);
+
+    #[inline]
+    pub const fn new(v: P) -> Self {
+        PhysicalUnit(v)
+    }
+}
+
+impl<P: Pixel> PhysicalUnit<P> {
+    #[inline]
+    pub fn from_logical<T: Into<LogicalUnit<X>>, X: Pixel>(logical: T, scale_factor: f64) -> Self {
+        logical.into().to_physical(scale_factor)
+    }
+
+    #[inline]
+    pub fn to_logical<X: Pixel>(&self, scale_factor: f64) -> LogicalUnit<X> {
+        assert!(validate_scale_factor(scale_factor));
+        LogicalUnit::new(self.0.into() * scale_factor).cast()
+    }
+
+    #[inline]
+    pub fn cast<X: Pixel>(&self) -> PhysicalUnit<X> {
+        PhysicalUnit(self.0.cast())
+    }
+}
+
+impl<P: Pixel, X: Pixel> From<X> for PhysicalUnit<P> {
+    fn from(v: X) -> PhysicalUnit<P> {
+        PhysicalUnit::new(v.cast())
+    }
+}
+
+impl<P: Pixel> From<PhysicalUnit<P>> for u8 {
+    fn from(v: PhysicalUnit<P>) -> u8 {
+        v.0.cast()
+    }
+}
+
+impl<P: Pixel> From<PhysicalUnit<P>> for u16 {
+    fn from(v: PhysicalUnit<P>) -> u16 {
+        v.0.cast()
+    }
+}
+
+impl<P: Pixel> From<PhysicalUnit<P>> for u32 {
+    fn from(v: PhysicalUnit<P>) -> u32 {
+        v.0.cast()
+    }
+}
+
+impl<P: Pixel> From<PhysicalUnit<P>> for i8 {
+    fn from(v: PhysicalUnit<P>) -> i8 {
+        v.0.cast()
+    }
+}
+
+impl<P: Pixel> From<PhysicalUnit<P>> for i16 {
+    fn from(v: PhysicalUnit<P>) -> i16 {
+        v.0.cast()
+    }
+}
+
+impl<P: Pixel> From<PhysicalUnit<P>> for i32 {
+    fn from(v: PhysicalUnit<P>) -> i32 {
+        v.0.cast()
+    }
+}
+
+impl<P: Pixel> From<PhysicalUnit<P>> for f32 {
+    fn from(v: PhysicalUnit<P>) -> f32 {
+        v.0.cast()
+    }
+}
+
+impl<P: Pixel> From<PhysicalUnit<P>> for f64 {
+    fn from(v: PhysicalUnit<P>) -> f64 {
+        v.0.cast()
+    }
+}
+
+/// A pixel unit that's either physical or logical.
+pub enum PixelUnit {
+    Physical(PhysicalUnit<i32>),
+    Logical(LogicalUnit<f64>),
+}
+
+impl PixelUnit {
+    /// Represents a minimum logical unit of [`f64::MAX`].
+    pub const MIN: PixelUnit = PixelUnit::Logical(LogicalUnit::new(f64::MIN));
+    /// Represents a logical unit of `0_f64`.
+    pub const ZERO: PixelUnit = PixelUnit::Logical(LogicalUnit::new(0.0));
+    /// Represents a maximum logical unit that is equal to [`f64::MAX`].
+    pub const MAX: PixelUnit = PixelUnit::Logical(LogicalUnit::new(f64::MAX));
+
+    pub fn new<S: Into<PixelUnit>>(unit: S) -> PixelUnit {
+        unit.into()
+    }
+
+    pub fn to_logical<P: Pixel>(&self, scale_factor: f64) -> LogicalUnit<P> {
+        match *self {
+            PixelUnit::Physical(unit) => unit.to_logical(scale_factor),
+            PixelUnit::Logical(unit) => unit.cast(),
+        }
+    }
+
+    pub fn to_physical<P: Pixel>(&self, scale_factor: f64) -> PhysicalUnit<P> {
+        match *self {
+            PixelUnit::Physical(unit) => unit.cast(),
+            PixelUnit::Logical(unit) => unit.to_physical(scale_factor),
+        }
+    }
+}
+
+impl<P: Pixel> From<PhysicalUnit<P>> for PixelUnit {
+    #[inline]
+    fn from(unit: PhysicalUnit<P>) -> PixelUnit {
+        PixelUnit::Physical(unit.cast())
+    }
+}
+
+impl<P: Pixel> From<LogicalUnit<P>> for PixelUnit {
+    #[inline]
+    fn from(unit: LogicalUnit<P>) -> PixelUnit {
+        PixelUnit::Logical(unit.cast())
+    }
 }
 
 /// A position represented in logical pixels.
@@ -728,6 +960,43 @@ mod tests {
         assert!(!validate_scale_factor(f64::INFINITY));
         assert!(!validate_scale_factor(f64::NAN));
         assert!(!validate_scale_factor(f64::NEG_INFINITY));
+    }
+
+    #[test]
+    fn test_logical_unity() {
+        let log_unit = LogicalUnit::new(1.0);
+        assert_eq!(log_unit.to_physical::<u32>(1.0), PhysicalUnit::new(1));
+        assert_eq!(log_unit.to_physical::<u32>(2.0), PhysicalUnit::new(2));
+        assert_eq!(log_unit.cast::<u32>(), LogicalUnit::new(1));
+        assert_eq!(
+            log_unit,
+            LogicalUnit::from_physical(PhysicalUnit::new(1.0), 1.0)
+        );
+        assert_eq!(
+            log_unit,
+            LogicalUnit::from_physical(PhysicalUnit::new(2.0), 2.0)
+        );
+        assert_eq!(LogicalUnit::from(2.0), LogicalUnit::new(2.0));
+
+        let x: f64 = log_unit.into();
+        assert_eq!(x, 1.0);
+    }
+
+    #[test]
+    fn test_physical_unit() {
+        assert_eq!(
+            PhysicalUnit::from_logical(LogicalUnit::new(1.0), 1.0),
+            PhysicalUnit::new(1)
+        );
+        assert_eq!(
+            PhysicalUnit::from_logical(LogicalUnit::new(2.0), 0.5),
+            PhysicalUnit::new(1)
+        );
+        assert_eq!(PhysicalUnit::from(2.0), PhysicalUnit::new(2.0,));
+        assert_eq!(PhysicalUnit::from(2.0), PhysicalUnit::new(2.0));
+
+        let x: f64 = PhysicalUnit::new(1).into();
+        assert_eq!(x, 1.0);
     }
 
     #[test]
