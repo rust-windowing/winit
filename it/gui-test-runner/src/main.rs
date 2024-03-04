6ec203584774d0eeff807143c483566fb2a9eb7e
John Nunley <dev@notgull.net>
2024-03-03 21:54:23 -0800

    it: Add runner and common tests
    Still needs some work, but the idea should be clear.
    
    Signed-off-by: John Nunley <dev@notgull.net>

---
+++ it/gui-test-runner/src/main.rs
@@ -0,0 +1,59 @@
+//! Runner for the `gui-test` system.
+
+use std::env;
+use std::process::{Command, Stdio};
+
+fn main() {
+    let mut args = env::args();
+
+    // Get the test crate name.
+    let test_crate = args.nth(1).unwrap();
+
+    // Get the target.
+    let target_tag = args.next().unwrap();
+
+    // Split the target into the target and the tag.
+    let (target, tag) = {
+        let mut split = target_tag.splitn(1, ':');
+        let target = split.next().unwrap();
+        let tag = split.next();
+        (target, tag)
+    };
+
+    // Get the current target.
+    let current_target = current_target();
+
+    // For now, we only support building for the current target.
+    assert_eq!(target, current_target);
+    assert!(tag.is_none());
+
+    // Just run the crate.
+    if !Command::new("cargo")
+        .args(["run", "-p", &test_crate])
+        .output()
+        .unwrap()
+        .status
+        .success()
+    {
+        panic!("test failed");
+    }
+}
+
+/// Get the current target.
+fn current_target() -> String {
+    let output = Command::new("rustc")
+        .arg("-vV")
+        .stdout(Stdio::piped())
+        .output()
+        .unwrap();
+
+    // Look for the line that starts with "host".
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    for line in stdout.lines() {
+        if let Some(host) = line.strip_prefix("host: ") {
+            return host.to_string();
+        }
+    }
+
+    panic!("failed to find host: line in rustc output")
+}
