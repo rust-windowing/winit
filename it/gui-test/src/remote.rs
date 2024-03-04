25fab64f6ebec34b03e1a2f28a19c725e4742ef9
John Nunley <dev@notgull.net>
2024-03-03 21:54:23 -0800

    it: Add runner and common tests
    Still needs some work, but the idea should be clear.
    
    Signed-off-by: John Nunley <dev@notgull.net>

--- it/gui-test/src/remote.rs
+++ it/gui-test/src/remote.rs
@@ -1,8 +1,8 @@
 //! Create a test handler that can be run remotely.
 
-use crate::TestHandler;
 use crate::stream::WriteHandler;
 use crate::user::UserHandler;
+use crate::TestHandler;
 
 use std::env;
 use std::net::TcpStream;
@@ -11,16 +11,17 @@ use std::net::TcpStream;
 pub fn handler() -> Box<dyn TestHandler + Send + 'static> {
     // If GUI_TEST_UNIX_STREAM is enabled, use that as a Unix stream.
     #[cfg(unix)]
-    if let Some(stream_path) = env::var_os("GUI_TEST_UNIX_STREAM")
-        .filter(|s| !s.is_empty()) {
-        let stream = std::os::unix::net::UnixStream::connect(stream_path).expect("unable to connect to gui-test handler");
+    if let Some(stream_path) = env::var_os("GUI_TEST_UNIX_STREAM").filter(|s| !s.is_empty()) {
+        let stream = std::os::unix::net::UnixStream::connect(stream_path)
+            .expect("unable to connect to gui-test handler");
         return Box::new(WriteHandler::new(stream));
     }
 
     // If GUI_TEST_TCP_STREAM is enabled, use that as a TCP stream.
     if let Some(tcp_ip) = env::var("GUI_TEST_TCP_STREAM")
         .ok()
-        .filter(|s| !s.is_empty()) {
+        .filter(|s| !s.is_empty())
+    {
         let stream = TcpStream::connect(tcp_ip).unwrap();
         return Box::new(WriteHandler::new(stream));
     }
