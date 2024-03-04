cfe1e3781d3f4d43e35614a6be2ae6965fad446e
John Nunley <dev@notgull.net>
2024-03-03 21:31:30 -0800

    it: Add basic handlers
    Signed-off-by: John Nunley <dev@notgull.net>

---
+++ it/gui-test/src/stream.rs
@@ -0,0 +1,34 @@
+//! Write events to an output stream.
+//! 
+//! The format is as follows:
+//! - First 8 bytes: big-endian length of payload.
+//! - Next {len} bytes: JSON payload to deserialize from.
+
+use crate::{TestEvent, TestHandler};
+use std::io::Write;
+
+/// A wrapper around a writer that sends data down a stream.
+pub struct WriteHandler<W> {
+    /// The inner writer.
+    writer: W,
+}
+
+impl<W: Write> WriteHandler<W> {
+    /// Create a new write handler.
+    pub fn new(writer: W) -> Self {
+        Self {
+            writer
+        }
+    }
+}
+
+impl<W: Write> TestHandler for WriteHandler<W> {
+    fn handle_test(&mut self, event: TestEvent) {
+        let payload = serde_json::to_vec(&event).unwrap();
+        let length = u64::to_be_bytes(payload.len() as u64);
+
+        // Write the payload to the stream.
+        self.writer.write_all(&length).unwrap();
+        self.writer.write_all(&payload).unwrap();
+    }
+}
