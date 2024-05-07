rust-windowing/winit

commit a5b08fc48ce67c3b0a93bcc96b47fbfd4823ec85

Send the event before waking up the message pump. (#3418)

src/platform_impl/windows/event_loop.rs
@@ -769,14 +769,13 @@ impl<T: 'static> Clone for EventLoopProxy<T> {
 
 impl<T: 'static> EventLoopProxy<T> {
     pub fn send_event(&self, event: T) -> Result<(), EventLoopClosed<T>> {
-        unsafe {
-            if PostMessageW(self.target_window, USER_EVENT_MSG_ID.get(), 0, 0) != false.into() {
-                self.event_send.send(event).ok();
-                Ok(())
-            } else {
-                Err(EventLoopClosed(event))
-            }
-        }
+        self.event_send
+            .send(event)
+            .map(|result| {
+                unsafe { PostMessageW(self.target_window, USER_EVENT_MSG_ID.get(), 0, 0) };
+                result
+            })
+            .map_err(|e| EventLoopClosed(e.0))
     }
 }
 

commit f1c7be48a971e54050544c3fbb537ec3ef1d8d69

Return an error when Windows event loop is busy
This was a regression introduced in #3418

This new error could be used to retry events at later time

This also removes `EventLoopClosed` struct
and replaces it with `EventLoopProxyError` enum

src/event_loop.rs
@@ -576,7 +576,7 @@ impl<T: 'static> EventLoopProxy<T> {
     /// Returns an `Err` if the associated [`EventLoop`] no longer exists.
     ///
     /// [`UserEvent(event)`]: Event::UserEvent
-    pub fn send_event(&self, event: T) -> Result<(), EventLoopClosed<T>> {
+    pub fn send_event(&self, event: T) -> Result<(), EventLoopProxyError<T>> {
         let _span = tracing::debug_span!("winit::EventLoopProxy::send_event",).entered();
 
         self.event_loop_proxy.send_event(event)
@@ -589,20 +589,31 @@ impl<T: 'static> fmt::Debug for EventLoopProxy<T> {
     }
 }
 
-/// The error that is returned when an [`EventLoopProxy`] attempts to wake up an [`EventLoop`] that
-/// no longer exists.
-///
-/// Contains the original event given to [`EventLoopProxy::send_event`].
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
-pub struct EventLoopClosed<T>(pub T);
+pub enum EventLoopProxyError<T> {
+    /// The error that is returned when an [`EventLoopProxy`] attempts to wake up an [`EventLoop`] that
+    /// no longer exists.
+    ///
+    /// Contains the original event given to [`EventLoopProxy::send_event`].
+    Closed(T),
+    /// The error that is returned when an [`EventLoopProxy`] attempts to wake up an [`EventLoop`] that
+    /// is busy handling events and can't accept new events because it reached its limit. This will happen on Windows for example,
+    /// if more than 10,000 events are sent in a short time.
+    Busy,
+}
 
-impl<T> fmt::Display for EventLoopClosed<T> {
+impl<T> fmt::Display for EventLoopProxyError<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str("Tried to wake up a closed `EventLoop`")
+        match self {
+            EventLoopProxyError::Closed(_) => f.write_str("Tried to wake up a closed `EventLoop`"),
+            EventLoopProxyError::Busy => {
+                f.write_str("Tried to wake up `EventLoop` while it is busy")
+            }
+        }
     }
 }
 
-impl<T: fmt::Debug> error::Error for EventLoopClosed<T> {}
+impl<T: fmt::Debug> error::Error for EventLoopProxyError<T> {}
 
 /// Control when device events are captured.
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Default)]

src/platform_impl/android/mod.rs
@@ -656,10 +656,10 @@ impl<T: 'static> Clone for EventLoopProxy<T> {
 }
 
 impl<T> EventLoopProxy<T> {
-    pub fn send_event(&self, event: T) -> Result<(), event_loop::EventLoopClosed<T>> {
+    pub fn send_event(&self, event: T) -> Result<(), event_loop::EventLoopProxyError<T>> {
         self.user_events_sender
             .send(event)
-            .map_err(|err| event_loop::EventLoopClosed(err.0))?;
+            .map_err(|err| event_loop::EventLoopProxyError::Closed(err.0))?;
         self.waker.wake();
         Ok(())
     }

src/platform_impl/ios/event_loop.rs
@@ -20,7 +20,7 @@ use crate::{
     error::EventLoopError,
     event::Event,
     event_loop::{
-        ActiveEventLoop as RootActiveEventLoop, ControlFlow, DeviceEvents, EventLoopClosed,
+        ActiveEventLoop as RootActiveEventLoop, ControlFlow, DeviceEvents, EventLoopProxyError,
     },
     platform::ios::Idiom,
     platform_impl::platform::app_state::{EventLoopHandler, HandlePendingUserEvents},
@@ -291,10 +291,10 @@ impl<T> EventLoopProxy<T> {
         }
     }
 
-    pub fn send_event(&self, event: T) -> Result<(), EventLoopClosed<T>> {
+    pub fn send_event(&self, event: T) -> Result<(), EventLoopProxyError<T>> {
         self.sender
             .send(event)
-            .map_err(|::std::sync::mpsc::SendError(x)| EventLoopClosed(x))?;
+            .map_err(|::std::sync::mpsc::SendError(x)| EventLoopProxyError::Closed(x))?;
         unsafe {
             // let the main thread know there's a new event
             CFRunLoopSourceSignal(self.source);

src/platform_impl/linux/mod.rs
@@ -23,7 +23,8 @@ use crate::{
     dpi::{PhysicalPosition, PhysicalSize, Position, Size},
     error::{EventLoopError, ExternalError, NotSupportedError, OsError as RootOsError},
     event_loop::{
-        ActiveEventLoop as RootELW, AsyncRequestSerial, ControlFlow, DeviceEvents, EventLoopClosed,
+        ActiveEventLoop as RootELW, AsyncRequestSerial, ControlFlow, DeviceEvents,
+        EventLoopProxyError,
     },
     icon::Icon,
     keyboard::Key,
@@ -831,7 +832,7 @@ impl<T> AsRawFd for EventLoop<T> {
 }
 
 impl<T: 'static> EventLoopProxy<T> {
-    pub fn send_event(&self, event: T) -> Result<(), EventLoopClosed<T>> {
+    pub fn send_event(&self, event: T) -> Result<(), EventLoopProxyError<T>> {
         x11_or_wayland!(match self; EventLoopProxy(proxy) => proxy.send_event(event))
     }
 }

src/platform_impl/linux/wayland/event_loop/proxy.rs
@@ -4,7 +4,7 @@ use std::sync::mpsc::SendError;
 
 use sctk::reexports::calloop::channel::Sender;
 
-use crate::event_loop::EventLoopClosed;
+use crate::event_loop::EventLoopProxyError;
 
 /// A handle that can be sent across the threads and used to wake up the `EventLoop`.
 pub struct EventLoopProxy<T: 'static> {
@@ -24,9 +24,9 @@ impl<T: 'static> EventLoopProxy<T> {
         Self { user_events_sender }
     }
 
-    pub fn send_event(&self, event: T) -> Result<(), EventLoopClosed<T>> {
+    pub fn send_event(&self, event: T) -> Result<(), EventLoopProxyError<T>> {
         self.user_events_sender
             .send(event)
-            .map_err(|SendError(error)| EventLoopClosed(error))
+            .map_err(|SendError(error)| EventLoopProxyError::Closed(error))
     }
 }

src/platform_impl/linux/x11/mod.rs
@@ -30,7 +30,9 @@ use x11rb::xcb_ffi::ReplyOrIdError;
 
 use crate::error::{EventLoopError, OsError as RootOsError};
 use crate::event::{Event, StartCause, WindowEvent};
-use crate::event_loop::{ActiveEventLoop as RootAEL, ControlFlow, DeviceEvents, EventLoopClosed};
+use crate::event_loop::{
+    ActiveEventLoop as RootAEL, ControlFlow, DeviceEvents, EventLoopProxyError,
+};
 use crate::platform::pump_events::PumpStatus;
 use crate::platform_impl::common::xkb::Context;
 use crate::platform_impl::platform::{min_timeout, WindowId};
@@ -760,10 +762,10 @@ impl ActiveEventLoop {
 }
 
 impl<T: 'static> EventLoopProxy<T> {
-    pub fn send_event(&self, event: T) -> Result<(), EventLoopClosed<T>> {
+    pub fn send_event(&self, event: T) -> Result<(), EventLoopProxyError<T>> {
         self.user_sender
             .send(event)
-            .map_err(|e| EventLoopClosed(e.0))
+            .map_err(|e| EventLoopProxyError::Closed(e.0))
     }
 }
 

src/platform_impl/macos/event_loop.rs
@@ -39,7 +39,9 @@ use crate::window::{CustomCursor as RootCustomCursor, CustomCursorSource};
 use crate::{
     error::EventLoopError,
     event::Event,
-    event_loop::{ActiveEventLoop as RootWindowTarget, ControlFlow, DeviceEvents, EventLoopClosed},
+    event_loop::{
+        ActiveEventLoop as RootWindowTarget, ControlFlow, DeviceEvents, EventLoopProxyError,
+    },
     platform::{macos::ActivationPolicy, pump_events::PumpStatus},
 };
 
@@ -496,10 +498,10 @@ impl<T> EventLoopProxy<T> {
         }
     }
 
-    pub fn send_event(&self, event: T) -> Result<(), EventLoopClosed<T>> {
+    pub fn send_event(&self, event: T) -> Result<(), EventLoopProxyError<T>> {
         self.sender
             .send(event)
-            .map_err(|mpsc::SendError(x)| EventLoopClosed(x))?;
+            .map_err(|mpsc::SendError(x)| EventLoopProxyError::Closed(x))?;
         unsafe {
             // let the main thread know there's a new event
             CFRunLoopSourceSignal(self.source);

src/platform_impl/orbital/event_loop.rs
@@ -772,10 +772,10 @@ pub struct EventLoopProxy<T: 'static> {
 }
 
 impl<T> EventLoopProxy<T> {
-    pub fn send_event(&self, event: T) -> Result<(), event_loop::EventLoopClosed<T>> {
+    pub fn send_event(&self, event: T) -> Result<(), event_loop::EventLoopProxyError<T>> {
         self.user_events_sender
             .send(event)
-            .map_err(|mpsc::SendError(x)| event_loop::EventLoopClosed(x))?;
+            .map_err(|mpsc::SendError(x)| event_loop::EventLoopProxyError::Closed(x))?;
 
         self.wake_socket.wake().unwrap();
 

src/platform_impl/web/event_loop/proxy.rs
@@ -2,7 +2,7 @@ use std::rc::Weak;
 use std::sync::mpsc::{SendError, Sender};
 
 use super::runner::Execution;
-use crate::event_loop::EventLoopClosed;
+use crate::event_loop::EventLoopProxyError;
 use crate::platform_impl::platform::r#async::Waker;
 
 pub struct EventLoopProxy<T: 'static> {
@@ -15,10 +15,10 @@ impl<T: 'static> EventLoopProxy<T> {
         Self { runner, sender }
     }
 
-    pub fn send_event(&self, event: T) -> Result<(), EventLoopClosed<T>> {
+    pub fn send_event(&self, event: T) -> Result<(), EventLoopProxyError<T>> {
         self.sender
             .send(event)
-            .map_err(|SendError(event)| EventLoopClosed(event))?;
+            .map_err(|SendError(event)| EventLoopProxyError::Closed(event))?;
         self.runner.wake();
         Ok(())
     }

src/platform_impl/windows/event_loop.rs
@@ -17,7 +17,7 @@ use std::{
     time::{Duration, Instant},
 };
 
-use crate::utils::Lazy;
+use crate::{event_loop::EventLoopProxyError, utils::Lazy};
 
 use windows_sys::Win32::{
     Devices::HumanInterfaceDevice::MOUSE_MOVE_RELATIVE,
@@ -74,7 +74,7 @@ use crate::{
         DeviceEvent, Event, Force, Ime, InnerSizeWriter, RawKeyEvent, Touch, TouchPhase,
         WindowEvent,
     },
-    event_loop::{ActiveEventLoop as RootAEL, ControlFlow, DeviceEvents, EventLoopClosed},
+    event_loop::{ActiveEventLoop as RootAEL, ControlFlow, DeviceEvents},
     keyboard::ModifiersState,
     platform::pump_events::PumpStatus,
     platform_impl::platform::{
@@ -776,14 +776,17 @@ impl<T: 'static> Clone for EventLoopProxy<T> {
 }
 
 impl<T: 'static> EventLoopProxy<T> {
-    pub fn send_event(&self, event: T) -> Result<(), EventLoopClosed<T>> {
+    pub fn send_event(&self, event: T) -> Result<(), EventLoopProxyError<T>> {
         self.event_send
             .send(event)
-            .map(|result| {
-                unsafe { PostMessageW(self.target_window, USER_EVENT_MSG_ID.get(), 0, 0) };
-                result
+            .map_err(|e| EventLoopProxyError::Closed(e.0))
+            .and_then(|result| {
+                if unsafe { PostMessageW(self.target_window, USER_EVENT_MSG_ID.get(), 0, 0) } == 0 {
+                    Err(EventLoopProxyError::Busy)
+                } else {
+                    Ok(result)
+                }
             })
-            .map_err(|e| EventLoopClosed(e.0))
     }
 }
 