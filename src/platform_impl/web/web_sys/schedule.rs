309dd54996af00811063577ea2b79fa248d52e72
daxpedda <daxpedda@gmail.com>
2024-06-18 21:45:06 +0200

    Minimize JS

--- src/platform_impl/web/web_sys/schedule.rs
+++ src/platform_impl/web/web_sys/schedule.rs
@@ -174,7 +174,7 @@ impl Schedule {
         F: 'static + FnMut(),
     {
         thread_local! {
-            static URL: ScriptUrl = ScriptUrl::new(include_str!("worker.js"));
+            static URL: ScriptUrl = ScriptUrl::new(include_str!("worker.min.js"));
             static WORKER: Worker = URL.with(|url| Worker::new(&url.0)).expect("`new Worker()` is not expected to fail with a local script");
         }
 
