2df43e341984b28d60153230afde1f7f8397a516
Mads Marquart <mads@marquart.dk>
2024-06-24 00:16:24 +0200

    Don't document reasoning behind multi wakeups

--- src/application.rs
+++ src/application.rs
@@ -84,10 +84,10 @@ pub trait ApplicationHandler {
 
     /// Called after a wake up is requested using [`EventLoopProxy::wake_up`].
     ///
-    /// Multiple calls to the aforementioned method may result in only a single `proxy_wake_up` call
-    /// for performance reasons. Furthermore, this callback may be emitted spuriously. For these
-    /// reasons, you should probably use some other tool like [`std::sync::mpsc`] if you want to
-    /// know the number of times that your application was _actually_ awoken, see the below example.
+    /// Multiple calls to the aforementioned method may result in only a single `proxy_wake_up`
+    /// call. Furthermore, this callback may be emitted spuriously. For these reasons, you should
+    /// probably use some other tool like [`std::sync::mpsc`] if you want to know the number of
+    /// times that your application was _actually_ awoken, see the below example.
     ///
     /// The order in which this is emitted in relation to other events is not guaranteed. The time
     /// at which this will be emitted is not guaranteed, only that it will happen "soon". That is,
