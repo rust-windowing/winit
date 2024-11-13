c330c7c48f79431a47695678c4301c72aad129ce
Piotr Podusowski <podusowski@gmail.com>
2024-11-13 12:43:59 +0100

    android: use show_soft_input to summon the keyboard
    Route it via the `Window::set_ime_allowed` like on iOS.

--- src/window.rs
+++ src/window.rs
@@ -1274,7 +1274,8 @@ impl Window {
     ///
     /// - **macOS:** IME must be enabled to receive text-input where dead-key sequences are
     ///   combined.
-    /// - **iOS / Android / Web / Orbital:** Unsupported.
+    /// - **iOS / Android:** This will show / hide the soft keyboard.
+    /// - **Web / Orbital:** Unsupported.
     /// - **X11**: Enabling IME will disable dead keys reporting during compose.
     ///
     /// [`Ime`]: crate::event::WindowEvent::Ime
