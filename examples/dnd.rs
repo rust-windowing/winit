b3510c2c20284af99a05065b3cf635b2a589c3d6
valadaptive <valadaptive@protonmail.com>
2025-01-08 12:40:14 -0500

    feat: DnD example/test

---
+++ examples/dnd.rs
@@ -0,0 +1,59 @@
+use std::error::Error;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::window::{
+    Window, WindowAttributes, WindowId,
+};
+
+#[path = "util/tracing.rs"]
+mod tracing;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    tracing::init();
+
+    let event_loop = EventLoop::new()?;
+
+    let app = Application::new();
+    Ok(event_loop.run_app(app)?)
+}
+
+/// Application state and event handling.
+struct Application {
+    window: Option<Box<dyn Window>>
+}
+
+impl Application {
+    fn new() -> Self {
+        Self {
+            window: None
+        }
+    }
+}
+
+impl ApplicationHandler for Application {
+    fn can_create_surfaces(&mut self, event_loop: &dyn ActiveEventLoop) {
+        let window_attributes = WindowAttributes::default().with_title("Drag and drop files on me!");
+        self.window = Some(event_loop.create_window(window_attributes).unwrap());
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &dyn ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            | WindowEvent::DragLeave
+            | WindowEvent::DragEnter { .. }
+            | WindowEvent::DragOver { .. }
+            | WindowEvent::DragDrop { .. } => {
+                println!("{:?}", event);
+            },
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            },
+            _ => {},
+        }
+    }
+}
