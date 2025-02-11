51ab7c646a319b31973ff32b13e480a0f3dace3e
Ryan Hileman <lunixbochs@gmail.com>
2025-02-10 22:05:52 -0800

    format

--- src/platform_impl/linux/wayland/window/mod.rs
+++ src/platform_impl/linux/wayland/window/mod.rs
@@ -383,8 +383,7 @@ impl CoreWindow for Window {
         None
     }
 
-    pub fn set_focusable(&self, focusable: bool) {
-    }
+    pub fn set_focusable(&self, focusable: bool) {}
 
     pub fn is_focusable(&self) -> Option<bool> {
         None
