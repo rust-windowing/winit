rust-windowing/winit

commit b1be34c6a040c643d3bd15193850ae2484743e39

fix cursor blinking when clicking decorations bar on Windows (#1852)

src/platform_impl/windows/event_loop.rs
@@ -845,7 +845,7 @@ unsafe fn public_window_callback_inner<T: 'static>(
         }
         winuser::WM_NCLBUTTONDOWN => {
             if wparam == winuser::HTCAPTION as _ {
-                winuser::PostMessageW(window, winuser::WM_MOUSEMOVE, 0, 0);
+                winuser::PostMessageW(window, winuser::WM_MOUSEMOVE, 0, lparam);
             }
             commctrl::DefSubclassProc(window, msg, wparam, lparam)
         }

commit 9f6b550f0406e681d0f7e1e8900625f7f505afa7

Fix a pause in the event loop when clicking the title bar on windows
When clicking the title bar on Windows, to drag the window, there is
a noticible pause in continuous redraw requests. This was fixed
in #839 and then regressed in #1852. The cursor blinks in both
cases and is unrelated. The regression made the blink happen after
the pause instead of immediately.

src/platform_impl/windows/event_loop.rs
@@ -1195,6 +1195,8 @@ unsafe fn public_window_callback_inner(
 
         WM_NCLBUTTONDOWN => {
             if wparam == HTCAPTION as _ {
+                // lparam must be zero to prevent the pause when clicking the title bar to drag
+                let lparam = 0;
                 unsafe { PostMessageW(window, WM_MOUSEMOVE, 0, lparam) };
             }
             result = ProcResult::DefWindowProc(wparam);