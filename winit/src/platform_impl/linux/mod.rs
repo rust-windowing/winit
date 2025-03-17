92099a02ded79aef925c9c5e58730b579bb0fea9
Mads Marquart <mads@marquart.dk>
2025-03-17 04:49:58 +0100

    winit/event_loop: Add register_app and run_app_never_return
    To allow users to explicitly choose the run semantics that they want.

--- winit/src/platform_impl/linux/mod.rs
+++ winit/src/platform_impl/linux/mod.rs
@@ -147,10 +147,6 @@ impl EventLoop {
         }
     }
 
-    pub fn run_app<A: ApplicationHandler>(self, app: A) -> Result<(), EventLoopError> {
-        x11_or_wayland!(match self; EventLoop(evlp) => evlp.run_app(app))
-    }
-
     pub fn run_app_on_demand<A: ApplicationHandler>(
         &mut self,
         app: A,
