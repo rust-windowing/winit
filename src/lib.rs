ed4d70fdd415aa4185c7f6e4e8dbd1052e8dca65
Kirill Chibisov <contact@kchibisov.com>
2025-04-06 15:41:46 +0900

    chore: fix clippy issues

--- src/lib.rs
+++ src/lib.rs
@@ -288,6 +288,8 @@
 #![cfg_attr(docsrs, feature(doc_auto_cfg, doc_cfg_hide), doc(cfg_hide(doc, docsrs)))]
 #![allow(clippy::missing_safety_doc)]
 #![warn(clippy::uninlined_format_args)]
+// TODO: wasm-binding needs to be updated for that to be resolved, for now just silence it.
+#![cfg_attr(web_platform, allow(unknown_lints, wasm_c_abi))]
 
 // Re-export DPI types so that users don't have to put it in Cargo.toml.
 #[doc(inline)]
