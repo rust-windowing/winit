ee2a0f86337355d492bac43dec45b6ad43309aac
Diggory Hardy <git@dhardy.name>
2025-11-03 17:57:41 +0000

    Remove variant RequestError::Ignored

--- winit-appkit/src/window_delegate.rs
+++ winit-appkit/src/window_delegate.rs
@@ -1285,8 +1285,9 @@ impl WindowDelegate {
     #[inline]
     pub fn drag_window(&self) -> Result<(), RequestError> {
         let mtm = MainThreadMarker::from(self);
-        let event =
-            NSApplication::sharedApplication(mtm).currentEvent().ok_or(RequestError::Ignored)?;
+        let event = NSApplication::sharedApplication(mtm)
+            .currentEvent()
+            .ok_or(NotSupportedError::new("event no longer current"))?;
         self.window().performWindowDragWithEvent(&event);
         Ok(())
     }
