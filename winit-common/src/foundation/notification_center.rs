5da5231aca390ec352e727ec089193dcfc9d1b63
Mads Marquart <mads@marquart.dk>
2026-02-17 16:51:10 +0100

    Add debug assertions that observers run on the expected thread

--- winit-common/src/foundation/notification_center.rs
+++ winit-common/src/foundation/notification_center.rs
@@ -11,19 +11,25 @@ use objc2_foundation::{
 ///
 /// This is used in Winit as an alternative to declaring an application delegate, as we want to
 /// give the user full control over those.
+///
+/// The handler will be run on the current thread.
 pub fn create_observer(
     center: &NSNotificationCenter,
     name: &NSNotificationName,
     handler: impl Fn(&NSNotification) + 'static,
 ) -> Retained<ProtocolObject<dyn NSObjectProtocol>> {
+    #[cfg(debug_assertions)]
+    let thread_id = std::thread::current().id();
     let block = RcBlock::new(move |notification: NonNull<NSNotification>| {
+        #[cfg(debug_assertions)]
+        assert_eq!(thread_id, std::thread::current().id(), "must run on posting thread");
         handler(unsafe { notification.as_ref() });
     });
     unsafe {
         center.addObserverForName_object_queue_usingBlock(
             Some(name),
             None, // No sender filter
-            None, // No queue, run on posting thread (i.e. main thread)
+            None, // No queue, run on posting thread
             &block,
         )
     }
