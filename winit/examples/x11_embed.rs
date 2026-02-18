b7bbb1241d4d995b2a1764745ca88468d9b91a9a
Mads Marquart <mads@marquart.dk>
2026-02-18 12:57:21 +0100

    Use tracing helper module in all examples

--- winit/examples/x11_embed.rs
+++ winit/examples/x11_embed.rs
@@ -11,6 +11,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     #[path = "util/fill.rs"]
     mod fill;
+    #[path = "util/tracing.rs"]
+    mod tracing;
 
     #[derive(Debug)]
     pub struct XEmbedDemo {
@@ -58,7 +60,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .ok_or("Expected a 32-bit X11 window ID as the first argument.")?
         .parse::<u32>()?;
 
-    tracing_subscriber::fmt::init();
+    tracing::init();
     let event_loop = EventLoop::new()?;
 
     Ok(event_loop.run_app(XEmbedDemo { parent_window_id, window: None })?)
