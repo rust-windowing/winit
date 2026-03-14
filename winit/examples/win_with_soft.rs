48ded9292bc259c538082f56b841517837e80142
Ali Baydur <67105447+yagizgil@users.noreply.github.com>
2026-03-14 13:59:50 +0300

    Update win_with_soft.rs

--- winit/examples/win_with_soft.rs
+++ winit/examples/win_with_soft.rs
@@ -1,16 +1,20 @@
 use std::error::Error;
 use std::num::NonZeroU32;
 use std::sync::Arc;
+use std::time::{Duration, Instant};
+
+use softbuffer::{Context, Surface};
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, EventLoop};
 use winit::window::{Window, WindowAttributes, WindowId};
 
-use softbuffer::{Context, Surface};
-
 struct App {
     window: Option<Arc<dyn Window>>,
     surface: Option<Surface<Arc<dyn Window>, Arc<dyn Window>>>,
+    last_frame_time: Instant,
+    frame_count: u32,
+    last_fps_print: Instant,
 }
 
 impl App {
@@ -18,6 +22,9 @@ impl App {
         Self {
             window: None,
             surface: None,
+            last_frame_time: Instant::now(),
+            frame_count: 0,
+            last_fps_print: Instant::now(),
         }
     }
 }
@@ -34,7 +41,12 @@ impl ApplicationHandler for App {
         self.surface = Some(surface);
     }
 
-    fn window_event(&mut self, event_loop: &dyn ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+    fn window_event(
+        &mut self,
+        event_loop: &dyn ActiveEventLoop,
+        _id: WindowId,
+        event: WindowEvent,
+    ) {
         match event {
             WindowEvent::CloseRequested => event_loop.exit(),
 
@@ -44,23 +56,38 @@ impl ApplicationHandler for App {
                     let h = NonZeroU32::new(size.height.max(1)).unwrap();
                     surface.resize(w, h).unwrap();
                 }
-            }
+            },
 
             WindowEvent::RedrawRequested => {
                 if let Some(surface) = self.surface.as_mut() {
+                    let now = Instant::now();
+                    self.frame_count += 1;
+
+                    if now.duration_since(self.last_fps_print) >= Duration::from_secs(1) {
+                        let fps = self.frame_count;
+                        println!("FPS: {}", fps);
+
+                        self.frame_count = 0;
+                        self.last_fps_print = now;
+                    }
+
                     let mut buffer = surface.next_buffer().expect("buffer except");
 
                     for (x, y, pixel) in buffer.pixels_iter() {
                         let red = (x % 255) as u8;
                         let green = (y % 255) as u8;
                         let blue = ((x * y) % 255) as u8;
-                        
+
                         *pixel = softbuffer::Pixel::new_rgb(red, green, blue);
                     }
 
                     buffer.present().unwrap();
+
+                    if let Some(window) = self.window.as_ref() {
+                        window.request_redraw();
+                    }
                 }
-            }
+            },
             _ => (),
         }
     }
@@ -68,10 +95,10 @@ impl ApplicationHandler for App {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let event_loop = EventLoop::new()?;
-    
+
     let app = Box::leak(Box::new(App::new()));
-    
-    event_loop.run_app(app)?; 
+
+    event_loop.run_app(app)?;
 
     Ok(())
 }
