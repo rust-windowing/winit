3a2b140d6538e9859aadc25b084395fdcec0b6b3
Mads Marquart <mads@marquart.dk>
2026-03-18 01:05:58 +0100

    WIP on making the safe area on iOS track the keyboard as well

--- winit-uikit/src/view_controller.rs
+++ winit-uikit/src/view_controller.rs
@@ -1,13 +1,26 @@
 use std::cell::Cell;
 
-use objc2::rc::Retained;
-use objc2::{DefinedClass, MainThreadMarker, available, define_class, msg_send};
-use objc2_foundation::NSObject;
+use block2::RcBlock;
+use objc2::rc::{Retained, Weak};
+use objc2::runtime::ProtocolObject;
+use objc2::{
+    DefinedClass, MainThreadMarker, MainThreadOnly, Message, available, define_class, msg_send,
+};
+use objc2_core_foundation::{CFTimeInterval, CGRect};
+use objc2_core_graphics::CGRectIntersection;
+use objc2_foundation::{
+    NSNotification, NSNotificationCenter, NSNumber, NSObject, NSObjectProtocol, NSValue,
+};
 use objc2_ui_kit::{
-    UIDevice, UIInterfaceOrientationMask, UIRectEdge, UIResponder, UIStatusBarStyle,
-    UIUserInterfaceIdiom, UIView, UIViewController,
+    UICoordinateSpace, UIDevice, UIEdgeInsets, UIInterfaceOrientationMask,
+    UIKeyboardAnimationCurveUserInfoKey, UIKeyboardAnimationDurationUserInfoKey,
+    UIKeyboardFrameBeginUserInfoKey, UIKeyboardFrameEndUserInfoKey,
+    UIKeyboardWillChangeFrameNotification, UIRectEdge, UIResponder, UIScreen, UIStatusBarStyle,
+    UIUserInterfaceIdiom, UIView, UIViewAnimating, UIViewAnimationCurve, UIViewAnimationOptions,
+    UIViewController, UIViewPropertyAnimator,
 };
 
+use crate::notification_center::create_observer;
 use crate::{ScreenEdge, StatusBarStyle, ValidOrientations, WindowAttributesIos};
 
 pub struct ViewControllerState {
@@ -16,6 +29,9 @@ pub struct ViewControllerState {
     prefers_home_indicator_auto_hidden: Cell<bool>,
     supported_orientations: Cell<UIInterfaceOrientationMask>,
     preferred_screen_edges_deferring_system_gestures: Cell<UIRectEdge>,
+    // Keep observer around (deallocating it stops notifications being posted).
+    keyboard_will_change_frame_observer:
+        Cell<Option<Retained<ProtocolObject<dyn NSObjectProtocol>>>>,
 }
 
 define_class!(
@@ -138,6 +154,7 @@ impl WinitViewController {
             prefers_home_indicator_auto_hidden: Cell::new(false),
             supported_orientations: Cell::new(UIInterfaceOrientationMask::All),
             preferred_screen_edges_deferring_system_gestures: Cell::new(UIRectEdge::empty()),
+            keyboard_will_change_frame_observer: Cell::new(None),
         });
         let this: Retained<Self> = unsafe { msg_send![super(this), init] };
 
@@ -153,8 +170,115 @@ impl WinitViewController {
             ios_attributes.preferred_screen_edges_deferring_system_gestures,
         );
 
+        let center = NSNotificationCenter::defaultCenter();
+
         this.setView(Some(view));
+
+        // Set up an observer that will make the `safeAreaRect` of the view update based on the soft
+        // keyboard's presence (in addition to everything else that the safe area depends on).
+        let controller_weak = Weak::from_retained(&this);
+        this.ivars().keyboard_will_change_frame_observer.set(Some(create_observer(
+            &center,
+            unsafe { UIKeyboardWillChangeFrameNotification },
+            move |notification| {
+                eprintln!("UIKeyboardWillChangeFrameNotification");
+                if let Some(controller) = controller_weak.load() {
+                    keyboard_will_change_frame(&controller, notification);
+                }
+            },
+        )));
 
         this
     }
+
+    /// The current keyboard frame, in the view's coordinate space.
+    pub(crate) fn current_keyboard_frame(&self) -> CGRect {
+        // TODO: Combine start_frame and end_frame with `animator.fractionComplete()` to produce
+        // current frame
+
+        // Convert keyboard frame to view coordinates.
+        let keyboard_frame = self
+            .view()
+            .unwrap()
+            .convertRect_fromCoordinateSpace(frame, &keyboard_screen.coordinateSpace());
+        todo!()
+    }
+}
+
+fn keyboard_will_change_frame(controller: &WinitViewController, notification: &NSNotification) {
+    let mtm = controller.mtm();
+    let controller = controller.retain();
+    let view = controller.view().unwrap();
+
+    // The notification's object is the screen the keyboard appears on (since iOS 16).
+    let keyboard_screen = notification
+        .object()
+        .map(|s| s.downcast::<UIScreen>().unwrap())
+        .unwrap_or_else(|| view.window().unwrap().screen());
+
+    let user_info = notification.userInfo().unwrap();
+    let begin_frame = user_info
+        .objectForKey(unsafe { UIKeyboardFrameBeginUserInfoKey })
+        .unwrap()
+        .downcast::<NSValue>()
+        .unwrap()
+        .get_rect()
+        .unwrap();
+    let end_frame = user_info
+        .objectForKey(unsafe { UIKeyboardFrameEndUserInfoKey })
+        .unwrap()
+        .downcast::<NSValue>()
+        .unwrap()
+        .get_rect()
+        .unwrap();
+    let duration: CFTimeInterval = user_info
+        .objectForKey(unsafe { UIKeyboardAnimationDurationUserInfoKey })
+        .unwrap()
+        .downcast::<NSNumber>()
+        .unwrap()
+        .doubleValue();
+    let curve_raw = user_info
+        .objectForKey(unsafe { UIKeyboardAnimationCurveUserInfoKey })
+        .unwrap()
+        .downcast::<NSNumber>()
+        .unwrap()
+        .integerValue();
+    let curve = UIViewAnimationCurve(curve_raw);
+
+    // If OS version is high enough, set up a `UIViewPropertyAnimator` to track the position of the
+    // keyboard.
+    if available!(ios = 10.0, tvos = 10.0, visionos = 1.0) {
+        let animator = UIViewPropertyAnimator::initWithDuration_curve_animations(
+            UIViewPropertyAnimator::alloc(mtm),
+            duration,
+            curve,
+            None,
+        );
+
+        animator.addCompletion(&RcBlock::new(move |_| {
+            controller.setAdditionalSafeAreaInsets(todo!());
+            // TODO: Might need to do further work to update the safe area when we
+            // move the view?
+
+            view.layoutIfNeeded();
+            // Safe area changed -> request redraw.
+            view.setNeedsDisplay();
+        }));
+
+        animator.startAnimation();
+    } else {
+        // Update immediately.
+        todo!()
+    }
+
+    // Not sufficient, `setAdditionalSafeAreaInsets` only updates at the start, it doesn't change
+    // `safeAreaInsets` continously during the keyboard open animation.
+    //
+    // UIView::animateWithDuration_delay_options_animations_completion(
+    //     duration,
+    //     0.0,
+    //     options,
+    //     None,
+    //     mtm,
+    // );
 }
--- winit/examples/window.rs
+++ winit/examples/window.rs
@@ -2,12 +2,16 @@
 
 use std::error::Error;
 
+use dpi::PhysicalInsets;
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, EventLoop};
 #[cfg(web_platform)]
 use winit::platform::web::WindowAttributesWeb;
-use winit::window::{Window, WindowAttributes, WindowId};
+use winit::window::{
+    ImeCapabilities, ImeEnableRequest, ImeRequest, ImeRequestData, Window, WindowAttributes,
+    WindowId,
+};
 
 #[path = "util/fill.rs"]
 mod fill;
@@ -17,6 +21,7 @@ mod tracing;
 #[derive(Default, Debug)]
 struct App {
     window: Option<Box<dyn Window>>,
+    prev_safe_area: PhysicalInsets<u32>,
 }
 
 impl ApplicationHandler for App {
@@ -33,11 +38,24 @@ impl ApplicationHandler for App {
                 event_loop.exit();
                 return;
             },
-        }
+        };
+
+        // Allow IME out of the box.
+        let enable_request =
+            ImeEnableRequest::new(ImeCapabilities::new(), ImeRequestData::default()).unwrap();
+        let enable_ime = ImeRequest::Enable(enable_request);
+
+        // Initial update
+        self.window.as_ref().unwrap().request_ime_update(enable_ime).unwrap();
     }
 
     fn window_event(&mut self, event_loop: &dyn ActiveEventLoop, _: WindowId, event: WindowEvent) {
-        println!("{event:?}");
+        let current_safe_area = self.window.as_ref().unwrap().safe_area();
+        if self.prev_safe_area != current_safe_area {
+            println!("safe area changed from {:?} to {:?}", self.prev_safe_area, current_safe_area);
+            self.prev_safe_area = current_safe_area;
+        }
+
         match event {
             WindowEvent::CloseRequested => {
                 println!("Close was requested; stopping");
@@ -47,6 +65,7 @@ impl ApplicationHandler for App {
                 self.window.as_ref().expect("resize event without a window").request_redraw();
             },
             WindowEvent::RedrawRequested => {
+                println!("redraw");
                 // Redraw the application.
                 //
                 // It's preferable for applications that do not render continuously to render in
@@ -64,8 +83,15 @@ impl ApplicationHandler for App {
                 // For contiguous redraw loop you can request a redraw from here.
                 // window.request_redraw();
             },
-            _ => (),
+            _ => {
+                println!("{event:?}");
+            },
         }
+    }
+
+    fn about_to_wait(&mut self, event_loop: &dyn ActiveEventLoop) {
+        let window = self.window.as_ref().expect("redraw request without a window");
+        // window.request_redraw();
     }
 }
 
