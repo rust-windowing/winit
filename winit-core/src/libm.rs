8833253740cfd68b76968053e7e4477843fb38c0
Zac Harrold <zac@harrold.com.au>
2026-07-14 19:31:09 +1000

    [`winit-core`]: Use `libm` for `wasm32v1-none`
    Certain `f64` methods are only available with `std`, so `libm` is required to fill the gaps.

---
+++ winit-core/src/libm.rs
@@ -0,0 +1,27 @@
+pub fn round(x: f64) -> f64 {
+    f64::round(x)
+}
+
+pub fn sin(x: f64) -> f64 {
+    f64::sin(x)
+}
+
+pub fn atan2(y: f64, x: f64) -> f64 {
+    f64::atan2(y, x)
+}
+
+pub fn tan(x: f64) -> f64 {
+    f64::tan(x)
+}
+
+pub fn atan(x: f64) -> f64 {
+    f64::atan(x)
+}
+
+pub fn hypot(x: f64, y: f64) -> f64 {
+    f64::hypot(x, y)
+}
+
+pub fn cos(x: f64) -> f64 {
+    f64::cos(x)
+}
