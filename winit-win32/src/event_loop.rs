04aa8bb6f25d3d8a4185e177de30fa41771db765
Martin Marmsoler <martin.marmsoler@gmail.com>
2026-08-12 07:38:30 +0200

    align with rest of the code

--- winit-win32/src/event_loop.rs
+++ winit-win32/src/event_loop.rs
@@ -1087,9 +1087,9 @@ unsafe fn reposition_owned_windows(parent: HWND) {
             return true.into(); // continue enumeration
         }
 
-        let userdata = unsafe { util::get_window_long(hwnd, GWL_USERDATA) };
-        if userdata != 0 {
-            let userdata = unsafe { &*(userdata as *const WindowData) };
+        let userdata_ptr = unsafe { util::get_window_long(hwnd, GWL_USERDATA) };
+        if !userdata_ptr.is_null() {
+            let userdata = unsafe { Box::from_raw(userdata_ptr) };
             if userdata.window_state_lock().window_flags.contains(WindowFlags::ANCHORED) {
                 window::reposition_owned_popup(hwnd, &userdata.window_state);
             }
